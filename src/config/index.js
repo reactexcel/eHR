4d96df491def0b1c18dc133cdbbefae706ff2892
reactexcel <ravi.etech2015@gmail.com>
2016-07-16 16:28:15 +0530

    new api url added

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,4 @@
 export const CONFIG = {
-	//api_url : 'http://localhost:3000/'
-	api_url : 'http://127.0.0.1/reactsjs/HR/1_api_hr/api.php',
-	//api_url : 'http://excellencemagentoblog.com/slack/1_api_hr/api.php',
+	api_url : 'http://excellencemagentoblog.com/slack_dev/API_HR/api.php',
 	jwt_secret_key : 'HR_APP'
 }
