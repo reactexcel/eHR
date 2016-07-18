9403b6f57ab7c0ed8ed6259163aebc8a02c3953d
reactexcel <ravi.etech2015@gmail.com>
2016-07-18 14:53:25 +0530

    deploy issue solved with change

--- config/index.js
+++ config/index.js
@@ -12,7 +12,9 @@ debug('Creating default configuration.')
 // Default Configuration
 // ========================================================
 const config = {
-  env : process.env.NODE_ENV || 'development',
+  //env : process.env.NODE_ENV || 'development',
+  env : process.env.NODE_ENV || 'production',
+
 
   // ----------------------------------
   // Project Structure
