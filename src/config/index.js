1ad2af2ab437953f77d076c07b15d0262e93dcb3
reactexcel <ravi.etech2015@gmail.com>
2016-07-25 17:18:41 +0530

    done all

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,5 @@
 export const CONFIG = {
-	api_url : 'http://excellencemagentoblog.com/slack_dev/attendance/API_HR/api.php',
+	api_url : 'http://excellencemagentoblog.com/slack_dev/attendance/API_HR/api.php',    // dev
+	//api_url : 'http://excellencemagentoblog.com/slack/attendance/API_HR/api.php',  // live
 	jwt_secret_key : 'HR_APP'
 }
