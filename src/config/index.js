f0be1433b56339138df4d2ef7db3f360b192cfe8
reactexcel <ravi.etech2015@gmail.com>
2016-08-02 11:49:57 +0530

    calendar issue fix completed

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 export const CONFIG = {
-	//api_url : 'http://excellencemagentoblog.com/slack_dev/attendance/API_HR/api.php',    // dev
-	api_url : 'http://excellencemagentoblog.com/slack/attendance/API_HR/api.php',  // live
+	api_url : 'http://excellencemagentoblog.com/slack_dev/attendance/API_HR/api.php',    // dev
+	//api_url : 'http://excellencemagentoblog.com/slack/attendance/API_HR/api.php',  // live
 	jwt_secret_key : 'HR_APP'
 }
