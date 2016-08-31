f22a0accebfd86c9011f2dee45c74ed6a7fbf031
reactexcel <ravi.etech2015@gmail.com>
2016-08-31 18:56:19 +0530

    manage salaries completed

--- src/components/generic/Menu.jsx
+++ src/components/generic/Menu.jsx
@@ -44,8 +44,8 @@ class Menu extends React.Component {
       let link_users_list = <Link to='/home'>Users</Link>
       let link_leaves_summary = <Link to='/leaves_summary'>Leaves Summary</Link>
       let link_manage_leaves = <Link to='/manage_leaves'>Manage Leaves</Link>
-      let link_manage_working_hours= <Link to='/manage_working_hours'>Manage Working Hours</Link>
-      let link_manage_user_working_hours = <Link to='/manage_user_working_hours'>Manage User Working Hours</Link>
+      let link_manage_working_hours= <Link to='/manage_working_hours'>Manage Office Hours</Link>
+      let link_manage_user_working_hours = <Link to='/manage_user_working_hours'>Manage Employee Hours</Link>
       let link_manage_salary = <Link to='/manage_salary'>Manage Salaries</Link>
       let link_manage_users = <Link to='/manage_users'>Manage Employees Profile</Link>
       let link_manage_clients = <Link to='/manage_clients'>Manage Clients</Link>
