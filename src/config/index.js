0aaf17494d31dc2c2a53d10e166b1abc61c7176b
reactexcel <ravi.etech2015@gmail.com>
2016-09-05 17:55:53 +0530

    final manage payslips done

--- src/config/index.js
+++ src/config/index.js
@@ -1,16 +1,16 @@
 export const CONFIG = {
 	//dev apis
-	// api_url : 'http://excellencemagentoblog.com/slack_dev/attendance/API_HR/api.php',
-	// other_api_url : 'http://excellencemagentoblog.com/slack_dev/attendance/sal_info/api.php',
-	// api_url_salary : 'http://excellencemagentoblog.com/slack_dev/attendance/sal_info',
-	// google_login_btn_page_url : "http://excellencemagentoblog.com/slack_dev/attendance/sal_info/google-api/examples/",
+	api_url : 'http://excellencemagentoblog.com/slack_dev/attendance/API_HR/api.php',
+	other_api_url : 'http://excellencemagentoblog.com/slack_dev/attendance/sal_info/api.php',
+	api_url_salary : 'http://excellencemagentoblog.com/slack_dev/attendance/sal_info',
+	google_login_btn_page_url : "http://excellencemagentoblog.com/slack_dev/attendance/sal_info/google-api/examples/",
 
 
 	//live apis
-	api_url : 'http://excellencemagentoblog.com/slack/attendance/API_HR/api.php',
-	other_api_url : 'http://excellencemagentoblog.com/slack/attendance/sal_info/api.php',
-	api_url_salary : 'http://excellencemagentoblog.com/slack/attendance/sal_info/salary_info.php',
-	google_login_btn_page_url : "http://excellencemagentoblog.com/slack/attendance/sal_info/google-api/examples/",
+	// api_url : 'http://excellencemagentoblog.com/slack/attendance/API_HR/api.php',
+	// other_api_url : 'http://excellencemagentoblog.com/slack/attendance/sal_info/api.php',
+	// api_url_salary : 'http://excellencemagentoblog.com/slack/attendance/sal_info/salary_info.php',
+	// google_login_btn_page_url : "http://excellencemagentoblog.com/slack/attendance/sal_info/google-api/examples/",
 	
 	jwt_secret_key : 'HR_APP'
 }
