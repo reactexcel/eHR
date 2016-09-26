4c3424176cc3e86699e250e5b94ccc093445ee9e
Your Name <you@example.com>
2016-09-26 17:58:03 +0530

    first commit

--- src/config/index.js
+++ src/config/index.js
@@ -7,12 +7,13 @@ export const CONFIG = {
 	google_login_btn_page_url : "http://excellencemagentoblog.com/slack_dev/hr/attendance/sal_info/google-api/drive_file/index.php?token="+token,
 	login_page_url : "http://excellencemagentoblog.com/slack_dev/hr",
 	upload_url : "http://excellencemagentoblog.com/slack_dev/hr/attendance/sal_info/upload_file.php",
+	
 
 	//live apis
 	 /*api_url : 'http://excellencemagentoblog.com/hr/attendance/API_HR/api.php',
 	 other_api_url : 'http://excellencemagentoblog.com/hr/attendance/sal_info/api.php',
 	 api_url_salary : 'http://excellencemagentoblog.com/hr/attendance/sal_info/salary_info.php',
-	 google_login_btn_page_url : "http://excellencemagentoblog.com/hr/attendance/sal_info/google-api/examples/",
+	 google_login_btn_page_url : "http://excellencemagentoblog.com/slack_dev/hr/attendance/sal_info/google-api/drive_file/index.php?token="+token,
 	 login_page_url : "http://excellencemagentoblog.com/hr",
 	 upload_url : "http://excellencemagentoblog.com/hr/attendance/sal_info/upload_file.php",
 	 */
