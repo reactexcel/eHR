231a73854b7e92dd9f7b8f48c940faab7c48119d
Your Name <you@example.com>
2016-09-27 12:13:57 +0530

    changes

--- src/components/attendance/SalaryList.jsx
+++ src/components/attendance/SalaryList.jsx
@@ -7,9 +7,45 @@ class SalaryList extends React.Component {
     constructor( props ){
         super( props );
         this.state = {
-          empList: this.props.empList
+          empList: this.props.empList,
+          sorting:[{
+            "className":"fa fa-fw fa-sort",
+            "headName":"empName",
+            "orderIn":"non"
+          },
+          {
+            "className":"fa fa-fw fa-sort",
+            "headName":"designation",
+            "orderIn":"non"
+          },
+          {
+            "className":"fa fa-fw fa-sort",
+            "headName":"salary",
+            "orderIn":"non"
+          },
+          {
+            "className":"fa fa-fw fa-sort",
+            "headName":"dateOfJoining",
+            "orderIn":"non"
+          },
+          {
+            "className":"fa fa-fw fa-sort",
+            "headName":"noOfDaysSinceJoined",
+            "orderIn":"non"
+          },
+          {
+            "className":"fa fa-fw fa-sort",
+            "headName":"preSalaryIncDetail",
+            "orderIn":"non"
+          },
+          {
+            "className":"fa fa-fw fa-sort",
+            "headName":"nextSallaryInc",
+            "orderIn":"non"
+          }]
         }
         this.search = this.search.bind( this )
+        this.sort = this.sort.bind(this)
     }
     search(searchValue){
       if(searchValue != "null" && searchValue != ""){
@@ -44,6 +80,69 @@ class SalaryList extends React.Component {
           empList:this.props.empList
         })
       }
+    }
+    sort(id){
+      let newSorting = []
+      let self = this;
+      _.forEach(this.state.sorting, function(ob, i) {
+        if(i==id){
+          let sortBy = ob.headName
+          let empList = self.state.empList
+          let orderIn = "non"
+          let clsName = "fa fa-fw fa-sort"
+          if(ob.orderIn=="non"){
+            empList.sort(function(i,j){
+                if(i[sortBy].toString().localeCompare(j[sortBy].toString())==1)
+                  return 1;
+                if(i[sortBy].toString().localeCompare(j[sortBy].toString())==0)
+                  return 0;
+                if(i[sortBy].toString().localeCompare(j[sortBy].toString())==-1)
+                  return -1;
+            })
+            orderIn = "asc"
+            clsName = "fa fa-fw fa-sort-asc"
+          }else if(ob.orderIn=="asc"){
+            empList.sort(function(i,j){
+                if(j[sortBy].toString().localeCompare(i[sortBy].toString())==1)
+                  return 1;
+                if(j[sortBy].toString().localeCompare(i[sortBy].toString())==0)
+                  return 0;
+                if(j[sortBy].toString().localeCompare(i[sortBy].toString())==-1)
+                  return -1;
+            })
+            orderIn = "desc"
+            clsName = "fa fa-fw fa-sort-desc"
+          }else{
+            empList.sort(function(i,j){
+                if(i[sortBy].toString().localeCompare(j[sortBy].toString())==1)
+                  return 1;
+                if(i[sortBy].toString().localeCompare(j[sortBy].toString())==0)
+                  return 0;
+                if(i[sortBy].toString().localeCompare(j[sortBy].toString())==-1)
+                  return -1;
+            })
+            orderIn = "asc"
+            clsName = "fa fa-fw fa-sort-asc"
+          }
+          newSorting.push({
+            "className":clsName,
+            "headName":ob.headName,
+            "orderIn":orderIn
+          })
+          self.setState({
+            empList:empList
+          })
+        }else{
+          newSorting.push({
+            "className":"fa fa-fw fa-sort",
+            "headName":ob.headName,
+            "orderIn":"non"
+          })
+        }
+      })
+     this.setState({
+      sorting:newSorting
+     })
     }
     render(){
       let row =  _.map( this.state.empList , ( emp, key ) => {
@@ -95,16 +194,30 @@ class SalaryList extends React.Component {
           <thead>
             <tr>
               <th >Image</th>
-              <th>Employe name</th>
-              <th>Designation</th>
-              <th>Salary</th>
+              <th>Employe name<i style={{'cursor':'pointer'}} ref="empName" id={0} className={this.state.sorting[0].className} onClick={()=>{
+                this.sort(this.refs.empName.id)
+              }}></i></th>
+              <th>Designation<i style={{'cursor':'pointer'}} ref="desig" id={1} className={this.state.sorting[1].className} onClick={()=>{
+                this.sort(this.refs.desig.id)
+              }}></i></th>
+              <th>Salary<i style={{'cursor':'pointer'}} ref="salary" id={2} className={this.state.sorting[2].className} onClick={()=>{
+                this.sort(this.refs.salary.id)
+              }}></i></th>
               <th style={{'textAlign':'center'}} >
               Holding Amount Details
               </th>
-              <th>Date of Joining</th>
-              <th >No of Days Since Joined</th>
-              <th >Previous Increment Details</th>
-              <th >Next Increment</th>
+              <th>Date of Joining<i style={{'cursor':'pointer'}} ref="dateOfJoining" id={3} className={this.state.sorting[3].className} onClick={()=>{
+                this.sort(this.refs.dateOfJoining.id)
+              }}></i></th>
+              <th >No of Days Since Joined<i style={{'cursor':'pointer'}} ref="noOfDaysSinceJoined" id={4} className={this.state.sorting[4].className} onClick={()=>{
+                this.sort(this.refs.noOfDaysSinceJoined.id)
+              }}></i></th>
+              <th >Previous Increment Details<i style={{'cursor':'pointer'}} ref="preSalaryIncDetail" id={5} className={this.state.sorting[5].className} onClick={()=>{
+                this.sort(this.refs.preSalaryIncDetail.id)
+              }}></i></th>
+              <th >Next Increment<i style={{'cursor':'pointer'}} ref="nextSallaryInc" id={6} className={this.state.sorting[6].className} onClick={()=>{
+                this.sort(this.refs.nextSallaryInc.id)
+              }}></i></th>
             </tr>
           </thead>
           <tbody>
