40e7510b280661d7dac45caac751ce8fd250587d
excellencetechnologies <vikas@excellencetechnologies.in>
2016-12-01 13:05:33 +0530

    changed to live

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 /* eslint key-spacing:0 spaced-comment:0 */
 import path from 'path'
 import _debug from 'debug'
-import { argv } from 'yargs'
+import {argv} from 'yargs'
 import ip from 'ip'
 
 const localip = ip.address()
@@ -11,38 +11,38 @@ debug('Creating default configuration.')
 // Default Configuration
 // ========================================================
 const config = {
-  env : process.env.NODE_ENV || 'development',
-  //env : process.env.NODE_ENV || 'production',
+  //env: process.env.NODE_ENV || 'development',
+  env: process.env.NODE_ENV || 'production',
   // ----------------------------------
   // Project Structure
   // ----------------------------------
-  path_base  : path.resolve(__dirname, '..'),
-  dir_client : 'src',
-  dir_dist   : 'dist',
-  dir_server : 'server',
-  dir_test   : 'tests',
+  path_base: path.resolve(__dirname, '..'),
+  dir_client: 'src',
+  dir_dist: 'dist',
+  dir_server: 'server',
+  dir_test: 'tests',
 
   // ----------------------------------
   // Server Configuration
   // ----------------------------------
-  server_host : localip, // use string 'localhost' to prevent exposure on local network
-  server_port : process.env.PORT || 3000,
+  server_host: localip, // use string 'localhost' to prevent exposure on local network
+  server_port: process.env.PORT || 3000,
 
   // ----------------------------------
   // Compiler Configuration
   // ----------------------------------
-  compiler_css_modules     : true,
-  compiler_devtool         : 'source-map',
-  compiler_hash_type       : 'hash',
-  compiler_fail_on_warning : false,
-  compiler_quiet           : false,
-  compiler_public_path     : '/',
-  compiler_stats           : {
-    chunks : false,
-    chunkModules : false,
-    colors : true
+  compiler_css_modules: true,
+  compiler_devtool: 'source-map',
+  compiler_hash_type: 'hash',
+  compiler_fail_on_warning: false,
+  compiler_quiet: false,
+  compiler_public_path: '/',
+  compiler_stats: {
+    chunks: false,
+    chunkModules: false,
+    colors: true
   },
-  compiler_vendor : [
+  compiler_vendor: [
     'babel-polyfill',
     'history',
     'react',
@@ -55,9 +55,13 @@ const config = {
   // ----------------------------------
   // Test Configuration
   // ----------------------------------
-  coverage_reporters : [
-    { type : 'text-summary' },
-    { type : 'lcov', dir : 'coverage' }
+  coverage_reporters: [
+    {
+      type: 'text-summary'
+    }, {
+      type: 'lcov',
+      dir: 'coverage'
+    }
   ]
 }
 
@@ -73,16 +77,16 @@ Edit at Your Own Risk
 // ------------------------------------
 // N.B.: globals added here must _also_ be added to .eslintrc
 config.globals = {
-  'process.env'  : {
-    'NODE_ENV' : JSON.stringify(config.env)
+  'process.env': {
+    'NODE_ENV': JSON.stringify(config.env)
   },
-  'NODE_ENV'     : config.env,
-  '__DEV__'      : config.env === 'development',
-  '__PROD__'     : config.env === 'production',
-  '__TEST__'     : config.env === 'test',
-  '__DEBUG__'    : config.env === 'development' && !argv.no_debug,
-  '__COVERAGE__' : !argv.watch && config.env === 'test',
-  '__BASENAME__' : JSON.stringify(process.env.BASENAME || '')
+  'NODE_ENV': config.env,
+  '__DEV__': config.env === 'development',
+  '__PROD__': config.env === 'production',
+  '__TEST__': config.env === 'test',
+  '__DEBUG__': config.env === 'development' && !argv.no_debug,
+  '__COVERAGE__': !argv.watch && config.env === 'test',
+  '__BASENAME__': JSON.stringify(process.env.BASENAME || '')
 }
 
 // ------------------------------------
@@ -90,28 +94,26 @@ config.globals = {
 // ------------------------------------
 const pkg = require('../package.json')
 
-config.compiler_vendor = config.compiler_vendor
-  .filter((dep) => {
-    if (pkg.dependencies[dep]) return true
+config.compiler_vendor = config.compiler_vendor.filter((dep) => {
+  if (pkg.dependencies[dep])
+    return true
 
-    debug(
-      `Package "${dep}" was not found as an npm dependency in package.json; ` +
-      `it wont be included in the webpack vendor bundle.
-       Consider removing it from vendor_dependencies in ~/config/index.js`
-    )
-  })
+  debug(`Package "${dep}" was not found as an npm dependency in package.json; ` + `it wont be included in the webpack vendor bundle.
+       Consider removing it from vendor_dependencies in ~/config/index.js`)
+})
 
 // ------------------------------------
 // Utilities
 // ------------------------------------
 const resolve = path.resolve
-const base = (...args) =>
-  Reflect.apply(resolve, null, [config.path_base, ...args])
+const base = (...args) => Reflect.apply(resolve, null, [
+  config.path_base, ...args
+])
 
 config.utils_paths = {
-  base   : base,
-  client : base.bind(null, config.dir_client),
-  dist   : base.bind(null, config.dir_dist)
+  base: base,
+  client: base.bind(null, config.dir_client),
+  dist: base.bind(null, config.dir_dist)
 }
 
 // ========================================================
--- src/config/index.js
+++ src/config/index.js
@@ -1,28 +1,22 @@
 const token = localStorage.getItem('hr_logged_user')
 export const CONFIG = {
-	//constants
-	ADMIN:'Admin',
-	HR:'Hr',
-	//dev apis
-	api_url : 'http://excellencemagentoblog.com/slack_dev/hr/attendance/API_HR/api.php',
-	other_api_url : 'http://excellencemagentoblog.com/slack_dev/hr/attendance/sal_info/api.php',
-	api_url_salary : 'http://excellencemagentoblog.com/slack_dev/hr/attendance/sal_info',
-	google_login_btn_page_url : "http://excellencemagentoblog.com/slack_dev/hr/attendance/sal_info/google-api/drive_file/index.php?token="+token,
-	login_page_url : "http://excellencemagentoblog.com/slack_dev/hr",
-	upload_url : "http://excellencemagentoblog.com/slack_dev/hr/attendance/sal_info/upload_file.php",
 
+  //dev apis
+  //api_url: 'http://excellencemagentoblog.com/slack_dev/hr/attendance/API_HR/api.php',
+  //other_api_url: 'http://excellencemagentoblog.com/slack_dev/hr/attendance/sal_info/api.php',
+  //api_url_salary: 'http://excellencemagentoblog.com/slack_dev/hr/attendance/sal_info',
+  //google_login_btn_page_url: "http://excellencemagentoblog.com/slack_dev/hr/attendance/sal_info/google-api/drive_file/index.php?token=" + token,
+  //login_page_url: "http://excellencemagentoblog.com/slack_dev/hr",
+  //upload_url: "http://excellencemagentoblog.com/slack_dev/hr/attendance/sal_info/upload_file.php",
 
+  //live apis
+  api_url: 'http://excellencemagentoblog.com/hr/attendance/API_HR/api.php',
+  other_api_url: 'http://excellencemagentoblog.com/hr/attendance/sal_info/api.php',
+  api_url_salary: 'http://excellencemagentoblog.com/hr/attendance/sal_info',
+  google_login_btn_page_url: "http://excellencemagentoblog.com/hr/attendance/sal_info/google-api/drive_file/index.php?token=" + token,
+  login_page_url: "http://excellencemagentoblog.com/hr",
+  upload_url: "http://excellencemagentoblog.com/hr/attendance/sal_info/upload_file.php",
 
-	//live apis
-	//  api_url : 'http://excellencemagentoblog.com/hr/attendance/API_HR/api.php',
-	//  other_api_url : 'http://excellencemagentoblog.com/hr/attendance/sal_info/api.php',
-  // 	api_url_salary : 'http://excellencemagentoblog.com/hr/attendance/sal_info',
-	//  google_login_btn_page_url : "http://excellencemagentoblog.com/hr/attendance/sal_info/google-api/drive_file/index.php?token="+token,
-	//  login_page_url : "http://excellencemagentoblog.com/hr",
-	//  upload_url : "http://excellencemagentoblog.com/hr/attendance/sal_info/upload_file.php",
-
-
-
-	jwt_secret_key : 'HR_APP'
+  jwt_secret_key: 'HR_APP'
 
 }
