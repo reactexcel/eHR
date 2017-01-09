96bb2e871c52393a7416b692ef9e436767b52c77
reactexcel <ravi.etech2015@gmail.com>
2017-01-09 12:09:14 +0530

    link policy document added to HR

--- src/components/generic/Menu.jsx
+++ src/components/generic/Menu.jsx
@@ -410,6 +410,9 @@ class Menu extends React.Component {
           <span>{link_manage_users}</span>
         </li>
         <li className="hidden-folded">
+          <span >{link_policy_documents}</span>
+        </li>
+        <li className="hidden-folded">
           <span>{link_logout}</span>
         </li>
 
