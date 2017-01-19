1c82bf3c6109658672dc936aa71932388b11d43d
reactexcel <ravi.etech2015@gmail.com>
2017-01-19 10:33:22 +0530

    date issue resolved

--- src/config/index.js
+++ src/config/index.js
@@ -19,16 +19,16 @@ export const CONFIG = {
   transfer_link: "http://dev.hr.excellencetechnologies.in/hr/attendance/sal_info/display_user_info.php",
 
   //live apis
-  // api_url: 'https://hr.excellencetechnologies.in/attendance/API_HR/api.php',
-  // other_api_url: 'https://hr.excellencetechnologies.in/attendance/sal_info/api.php',
-  // api_url_salary: 'https://hr.excellencetechnologies.in/attendance/sal_info',
-  // google_login_btn_page_url: "https://hr.excellencetechnologies.in/attendance/sal_info/google-api/drive_file/index.php?token=" + token,
-  // login_page_url: "https://hr.excellencetechnologies.in/",
-  // upload_url: "https://hr.excellencetechnologies.in/attendance/sal_info/upload_file.php",
-  // upload_attendance_url: "https://hr.excellencetechnologies.in/attendance/upload_form.php",
-  // pdf_url : 'https://hr.excellencetechnologies.in/attendance/sal_info/',
-  // upload_email_attachment : 'https://hr.excellencetechnologies.in/attendance/sal_info/upload_file_attachment.php',
-  // transfer_link: "https://hr.excellencetechnologies.in/attendance/sal_info/display_user_info.php",
+  //api_url: 'https://hr.excellencetechnologies.in/attendance/API_HR/api.php',
+  //other_api_url: 'https://hr.excellencetechnologies.in/attendance/sal_info/api.php',
+  //api_url_salary: 'https://hr.excellencetechnologies.in/attendance/sal_info',
+  //google_login_btn_page_url: "https://hr.excellencetechnologies.in/attendance/sal_info/google-api/drive_file/index.php?token=" + token,
+  //login_page_url: "https://hr.excellencetechnologies.in/",
+  //upload_url: "https://hr.excellencetechnologies.in/attendance/sal_info/upload_file.php",
+  //upload_attendance_url: "https://hr.excellencetechnologies.in/attendance/upload_form.php",
+  //pdf_url : 'https://hr.excellencetechnologies.in/attendance/sal_info/',
+  //upload_email_attachment : 'https://hr.excellencetechnologies.in/attendance/sal_info/upload_file_attachment.php',
+  //transfer_link: "https://hr.excellencetechnologies.in/attendance/sal_info/display_user_info.php",
 
   jwt_secret_key: 'HR_APP'
 
