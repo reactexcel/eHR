f6665bca329a409a1ba65783f2ebe040bd85389b
excellencetechnologies <excellencetechnologies08@gmail.com>
2017-05-02 18:39:44 +0530

    UserAccessIssue,inventory

--- src/config/index.js
+++ src/config/index.js
@@ -6,31 +6,31 @@ export const CONFIG = {
   EMPLOYEE: 'Employee',
   WORKING_DAY: 'WORKING_DAY',
   LEAVE_DAY: 'LEAVE_DAY',
-  //dev apis
+  // dev apis
   api_url: 'http://dev.hr.excellencetechnologies.in/hr/attendance/API_HR/api.php',
   other_api_url: 'http://dev.hr.excellencetechnologies.in/hr/attendance/sal_info/api.php',
   api_url_salary: 'http://dev.hr.excellencetechnologies.in/hr/attendance/sal_info',
-  google_login_btn_page_url: "http://dev.hr.excellencetechnologies.in/hr/attendance/sal_info/google-api/drive_file/index.php?token=" + token,
-  login_page_url: "http://dev.hr.excellencetechnologies.in/hr/",
-  upload_url: "http://dev.hr.excellencetechnologies.in/hr/attendance/sal_info/upload_file.php",
-  upload_leave_url: "http://dev.hr.excellencetechnologies.in/hr/attendance/API_HR/upload_leave_doc.php",
-  upload_attendance_url: "http://dev.hr.excellencetechnologies.in/hr/attendance/upload_form.php",
+  google_login_btn_page_url: 'http://dev.hr.excellencetechnologies.in/hr/attendance/sal_info/google-api/drive_file/index.php?token=' + token,
+  login_page_url: 'http://dev.hr.excellencetechnologies.in/hr/',
+  upload_url: 'http://dev.hr.excellencetechnologies.in/hr/attendance/sal_info/upload_file.php',
+  upload_leave_url: 'http://dev.hr.excellencetechnologies.in/hr/attendance/API_HR/upload_leave_doc.php',
+  upload_attendance_url: 'http://dev.hr.excellencetechnologies.in/hr/attendance/upload_form.php',
   pdf_url: 'http://dev.hr.excellencetechnologies.in/hr/attendance/sal_info/',
   upload_email_attachment: 'http://dev.hr.excellencetechnologies.in/hr/attendance/sal_info/upload_file_attachment.php',
-  transfer_link: "http://dev.hr.excellencetechnologies.in/hr/attendance/sal_info/display_user_info.php",
+  transfer_link: 'http://dev.hr.excellencetechnologies.in/hr/attendance/sal_info/display_user_info.php',
 
-  //live apis
+  // live apis
   // api_url: 'https://hr.excellencetechnologies.in/attendance/API_HR/api.php',
   // other_api_url: 'https://hr.excellencetechnologies.in/attendance/sal_info/api.php',
   // api_url_salary: 'https://hr.excellencetechnologies.in/attendance/sal_info',
-  // google_login_btn_page_url: "https://hr.excellencetechnologies.in/attendance/sal_info/google-api/drive_file/index.php?token=" + token,
-  // login_page_url: "https://hr.excellencetechnologies.in/",
-  // upload_url: "https://hr.excellencetechnologies.in/attendance/sal_info/upload_file.php",
-  // upload_leave_url: "http://hr.excellencetechnologies.in/attendance/API_HR/upload_leave_doc.php",
-  // upload_attendance_url: "https://hr.excellencetechnologies.in/attendance/upload_form.php",
+  // google_login_btn_page_url: 'https://hr.excellencetechnologies.in/attendance/sal_info/google-api/drive_file/index.php?token=' + token,
+  // login_page_url: 'https://hr.excellencetechnologies.in/',
+  // upload_url: 'https://hr.excellencetechnologies.in/attendance/sal_info/upload_file.php',
+  // upload_leave_url: 'http://hr.excellencetechnologies.in/attendance/API_HR/upload_leave_doc.php',
+  // upload_attendance_url: 'https://hr.excellencetechnologies.in/attendance/upload_form.php',
   // pdf_url: 'https://hr.excellencetechnologies.in/attendance/sal_info/',
   // upload_email_attachment: 'https://hr.excellencetechnologies.in/attendance/sal_info/upload_file_attachment.php',
-  // transfer_link: "https://hr.excellencetechnologies.in/attendance/sal_info/display_user_info.php",
+  // transfer_link: 'https://hr.excellencetechnologies.in/attendance/sal_info/display_user_info.php',
 
   jwt_secret_key: 'HR_APP'
 
