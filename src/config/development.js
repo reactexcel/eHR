2fd93e6fe07dec034db49e10451f2b0bd03d95d1
reactexcel <mohit@excellencetechnologies.in>
2017-06-28 11:30:39 +0530

    undo the dev link to dev.hr

--- src/config/development.js
+++ src/config/development.js
@@ -14,7 +14,7 @@ const CONFIG = {
   other_api_url: 'http://dev.hr.excellencetechnologies.in/hr/attendance/sal_info/api.php',
   api_url_salary: 'http://dev.hr.excellencetechnologies.in/hr/attendance/sal_info',
   google_login_btn_page_url: 'http://dev.hr.excellencetechnologies.in/hr/attendance/sal_info/google-api/drive_file/index.php?token=' + token,
-  login_page_url: 'http://localhost:3000/',
+  login_page_url: 'http://dev.hr.excellencetechnologies.in/hr/',
   upload_url: 'http://dev.hr.excellencetechnologies.in/hr/attendance/sal_info/upload_file.php',
   upload_leave_url: 'http://dev.hr.excellencetechnologies.in/hr/attendance/API_HR/upload_leave_doc.php',
   upload_attendance_url: 'http://dev.hr.excellencetechnologies.in/hr/attendance/upload_form.php',
