03b14f6ed714798b492ea20573de3df3a9a5c581
reactexcel <arun.etech2011@gmail.com>
2017-07-20 15:20:10 +0530

    bugs resolved

--- src/modules/manageRoles/components/AddRolesForm.jsx
+++ src/modules/manageRoles/components/AddRolesForm.jsx
@@ -1,29 +1,50 @@
 import React from 'react';
+import _ from 'lodash';
 import TextField from 'material-ui/TextField';
 import PropTypes from 'prop-types';
 import Dialog from 'material-ui/Dialog';
-import { Button, ButtonRaised } from 'components/generic/buttons/';
+import {Button, ButtonRaised} from 'components/generic/buttons/';
 
 export default class AddRolesForm extends React.Component {
   constructor (props) {
     super(props);
     this.state = {
-      open: false,
-      name: '',
-      description: ''
+      open:        false,
+      name:        '',
+      description: '',
+      error:       {name: '', description: ''}
     };
     this.handleOpen = this.handleOpen.bind(this);
     this.handleClose = this.handleClose.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
   handleOpen () {
     this.setState({
-      open: true,
-      name: '',
+      open:        true,
+      name:        '',
       description: ''
     });
   }
   handleClose () {
     this.setState({open: false});
+  }
+  handleSubmit () {
+    let {name, description} = this.state;
+    let error = {name: '', description: ''};
+    let valid = true;
+    if (_.isEmpty(name)) {
+      valid = false;
+      error.name = 'This field is required.';
+    }
+    if (_.isEmpty(description)) {
+      valid = false;
+      error.description = 'This field is required.';
+    }
+    this.setState({error});
+    if (valid) {
+      this.props.callAddNewRole({name, description});
+      this.handleClose();
+    }
   }
   render () {
     return (
@@ -41,15 +62,17 @@ export default class AddRolesForm extends React.Component {
             floatingLabelFixed fullWidth
             onChange={(e) => (this.setState({name: e.target.value}))}
             value={this.state.name}
+            errorText={this.state.error.name}
           />
           <TextField
             floatingLabelText="Description"
             floatingLabelFixed fullWidth
             onChange={(e) => (this.setState({description: e.target.value}))}
             value={this.state.description}
+            errorText={this.state.error.description}
           />
           <br />
-          <ButtonRaised className="col-md-12 m-b-sm indigo" onClick={() => { this.props.callAddNewRole(this.state); this.handleClose(); }} label={'Submit New Role'} />
+          <ButtonRaised className="col-md-12 m-b-sm indigo" onClick={() => this.handleSubmit()} label={'Submit New Role'} />
         </Dialog>
       </div>
     );
