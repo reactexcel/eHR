110b5fe0bfb86bbd98b24a29bc422234148d550a
reactexcel <arun.etech2011@gmail.com>
2017-08-01 13:28:17 +0530

    Responsive Holidays Module.

--- src/components/holidays/HolidaysList.jsx
+++ src/components/holidays/HolidaysList.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import * as _ from 'lodash';
 import PropTypes from 'prop-types';
 
 const HolidaysList = ({holidays}) => {
@@ -9,35 +10,33 @@ const HolidaysList = ({holidays}) => {
         <td>{holiday.date}</td>
         <td>{holiday.name}</td>
       </tr>
-    )
+    );
   });
   return (
     <div className="row">
       <div className="col-12">
         <div className="box">
-          <div className="box">
-            <div className="box-divider m-a-0"></div>
-            <table className="table">
-              <thead>
-                <tr>
-                  <th>Month</th>
-                  <th>Date</th>
-                  <th>Holiday</th>
-                </tr>
-              </thead>
-              <tbody>
-                {holidaysList}
-              </tbody>
-            </table>
-          </div>
+          <div className="box-divider m-a-0"></div>
+          <table key='' className="table table-hover">
+            <thead className="col-12">
+              <tr>
+                <th>Month</th>
+                <th>Date</th>
+                <th>Holiday</th>
+              </tr>
+            </thead>
+            <tbody>
+              {holidaysList}
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 HolidaysList.PropTypes = {
   holidays: PropTypes.array.isRequired
-}
+};
 
 export default HolidaysList;
