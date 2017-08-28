643ec606578c25ee27e307efe51b038f7e5b5be7
reactexcel <arun.etech2011@gmail.com>
2017-08-28 13:54:10 +0530

    Responsive mySalray

--- src/modules/salary/components/userSalary/SalaryDetails.jsx
+++ src/modules/salary/components/userSalary/SalaryDetails.jsx
@@ -1,48 +1,48 @@
 import React from 'react';
 import {Button} from 'components/generic/buttons';
+import TextField from 'material-ui/TextField';
 
 class SalaryDetails extends React.Component {
-  constructor(props) {
+  constructor (props) {
     super(props);
     this.state = {
       holdingAmount: ''
-    }
+    };
   }
-  componentWillReceiveProps(props) {
+  componentWillReceiveProps (props) {
     this.setState({holdingAmount: props.data});
   }
-  render() {
-    let F_total_salary = ""
-    let F_leaves_allocated = ""
-    let F_applicable_from = ""
-    if (typeof typeof this.props.data != 'undefined' && typeof this.props.data.test != 'undefined') {
-      F_total_salary = this.props.data.test.total_salary
-      F_leaves_allocated = this.props.data.test.leaves_allocated
-      F_applicable_from = this.props.data.test.applicable_from
+  render () {
+    let fTotalSalary = '';
+    let fLeaveAllocated = '';
+    let fApplicableFrom = '';
+    if (typeof typeof this.props.data !== 'undefined' && typeof this.props.data.test !== 'undefined') {
+      fTotalSalary = this.props.data.test.total_salary;
+      fLeaveAllocated = this.props.data.test.leaves_allocated;
+      fApplicableFrom = this.props.data.test.applicable_from;
     }
-
     return (
       <div>
-        <hr/>
+        <hr />
         <div className="row">
           <div className="col-xs-12">
             <div className="p-y text-center text-sm-left">
               <a href="" className="inline p-x text-center">
-                <span className="h4 block m-a-0">{F_total_salary}</span>
+                <span className="h4 block m-a-0">{fTotalSalary}</span>
                 <small className="text-xs text-muted">Total Salary</small>
               </a>
               <a href="" className="inline p-x b-l b-r text-center">
-                <span className="h4 block m-a-0">{F_leaves_allocated}</span>
+                <span className="h4 block m-a-0">{fLeaveAllocated}</span>
                 <small className="text-xs text-muted">Leaves Allocated</small>
               </a>
               <a href="" className="inline p-x text-center">
-                <span className="h4 block m-a-0">{F_applicable_from}</span>
+                <span className="h4 block m-a-0">{fApplicableFrom}</span>
                 <small className="text-xs text-muted">Applicable From</small>
               </a>
             </div>
           </div>
         </div>
-        <hr/>
+        <hr />
         <div className="row">
           <div className="col-xs-6 col-sm-4">
             <div className="box p-a">
@@ -178,7 +178,9 @@ class SalaryDetails extends React.Component {
             <div className="box p-a">
               <div className="clear">
                 <h5 className="m-a-0 _500">
-                  {this.props.data.Arrears}<span className="text-sm"></span>
+                  {this.props.data.Arrears}
+                  <span className="text-sm">
+                  </span>
                 </h5>
                 <small className="text-muted">
                   Arrears
@@ -189,18 +191,26 @@ class SalaryDetails extends React.Component {
           <div className="col-xs-6 col-sm-4">
             <div className="box p-a">
               <div className="clear">
-                <small className="text-muted">
-                  Holding Amount
-                </small>
-                <input type="text" style={{'width': 84}} onChange={(evt) => {this.setState({holdingAmount: evt.target.value})}} value={this.state.holdingAmount} />
-                <Button style={{'marginTop': 10, 'marginLeft': 5}} type="button" label="Submit" />
+                <div>
+                  <small className="text-muted" >{'Holding Ammount'}</small>
+                </div>
+                <TextField
+                  id="hold1"
+                  fullWidth
+                  onChange={(evt) => { this.setState({holdingAmount: evt.target.value}); }}
+                  value={this.state.holdingAmount}
+                />
+                <input type="button"
+                  id="addHolding" value="Add Holding"
+                  className="col-xs-12 md-btn md-raised info button-style-l">
+                </input>
               </div>
             </div>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default SalaryDetails
+export default SalaryDetails;
