a82cb91a83cbe2d646f4c7f3251f4cb6b66cad57
reactexcel <arun.etech2011@gmail.com>
2017-08-29 15:40:02 +0530

    formatted

--- src/modules/salary/components/userSalary/SalaryDetails.jsx
+++ src/modules/salary/components/userSalary/SalaryDetails.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import {Button} from 'components/generic/buttons';
 import TextField from 'material-ui/TextField';
 
 class SalaryDetails extends React.Component {
@@ -33,7 +32,7 @@ class SalaryDetails extends React.Component {
                 <span className="h4 block m-a-0">{fTotalSalary}</span>
                 <small className="text-xs text-muted">Total Salary</small>
               </a>
-                <a href="" className="inline salary-p-x b-l b-r text-center">
+              <a href="" className="inline salary-p-x b-l b-r text-center">
                 <span className="h4 block m-a-0">{fLeaveAllocated}</span>
                 <small className="text-xs text-muted">Leaves Allocated</small>
               </a>
@@ -45,165 +44,159 @@ class SalaryDetails extends React.Component {
           </div>
         </div>
         <hr />
-
-          <div className="col-xs-6">
-            <div className="box p-a">
-              <div className="clear">
-                <h5 className="m-a-0 _500">
-                  {this.props.data.Basic}<span className="text-sm"></span>
-                </h5>
-                <small className="text-muted">
-                  Basic
-                </small>
-              </div>
-            </div>
-          </div>
-          <div className="col-xs-6">
-            <div className="box p-a">
-              <div className="clear">
-                <h5 className="m-a-0 _500">
-                  {this.props.data.EPF}<span className="text-sm"></span>
-                </h5>
-                <small className="text-muted">
-                  EPF
-                </small>
-              </div>
-            </div>
-          </div>
-
-          <div className="col-xs-6">
-            <div className="box p-a">
-              <div className="clear">
-                <h5 className="m-a-0 _500">
-                  {this.props.data.HRA}<span className="text-sm"></span>
-                </h5>
-                <small className="text-muted">
-                  HRA
-                </small>
-              </div>
-            </div>
-          </div>
-          <div className="col-xs-6">
-            <div className="box p-a">
-              <div className="clear">
-                <h5 className="m-a-0 _500">
-                  {this.props.data.Loan}<span className="text-sm"></span>
-                </h5>
-                <small className="text-muted">
-                  Loan
-                </small>
-              </div>
-            </div>
-          </div>
-
-          <div className="col-xs-6">
-            <div className="box p-a">
-              <div className="clear">
-                <h5 className="m-a-0 _500">
-                  {this.props.data.Conveyance}<span className="text-sm"></span>
-                </h5>
-                <small className="text-muted">
-                  Conveyance
-                </small>
-              </div>
-            </div>
-          </div>
-          <div className="col-xs-6">
-            <div className="box p-a">
-              <div className="clear">
-                <h5 className="m-a-0 _500">
-                  {this.props.data.Advance}<span className="text-sm"></span>
-                </h5>
-                <small className="text-muted">
-                  Advance
-                </small>
-              </div>
-            </div>
-          </div>
-
-          <div className="col-xs-6">
-            <div className="box p-a">
-              <div className="clear">
-                <h5 className="m-a-0 _500">
-                  {this.props.data.Medical_Allowance}<span className="text-sm"></span>
-                </h5>
-                <small className="text-muted">
-                  Medical Allowance
-                </small>
-              </div>
-            </div>
-          </div>
-          <div className="col-xs-6">
-            <div className="box p-a">
-              <div className="clear">
-                <h5 className="m-a-0 _500">
-                  {this.props.data.Misc_Deductions}<span className="text-sm"></span>
-                </h5>
-                <small className="text-muted">
-                  Misc Deductions
-                </small>
-              </div>
-            </div>
-          </div>
-
-          <div className="col-xs-6">
-            <div className="box p-a">
-              <div className="clear">
-                <h5 className="m-a-0 _500">
-                  {this.props.data.Special_Allowance}<span className="text-sm"></span>
-                </h5>
-                <small className="text-muted">
-                  Special Allowance
-                </small>
-              </div>
-            </div>
-          </div>
-          <div className="col-xs-6">
-            <div className="box p-a">
-              <div className="clear">
-                <h5 className="m-a-0 _500">
-                  {this.props.data.TDS}<span className="text-sm"></span>
-                </h5>
-                <small className="text-muted">
-                  TDS
-                </small>
-              </div>
-            </div>
-          </div>
-
-          <div className="col-xs-6">
-            <div className="box p-a">
-              <div className="clear">
-                <h5 className="m-a-0 _500">
-                  {this.props.data.Arrears}
-                  <span className="text-sm">
-                  </span>
-                </h5>
-                <small className="text-muted">
-                  Arrears
-                </small>
-              </div>
-            </div>
-          </div>
         <div className="col-xs-6">
           <div className="box p-a">
-              <div className="clear">
-                <div>
-                  <small className="text-muted" >{'Holding Ammount'}</small>
-                </div>
-                <TextField
-                  id="hold1"
-                  fullWidth
-                  onChange={(evt) => { this.setState({holdingAmount: evt.target.value}); }}
-                  value={this.state.holdingAmount}
-                />
-                <input type="button"
-                  id="addHolding" value="Add Holding"
-                  className="col-xs-12 md-btn md-raised info button-style-l">
-                </input>
-              </div>
+            <div className="clear">
+              <h5 className="m-a-0 _500">
+                {this.props.data.Basic}<span className="text-sm"></span>
+              </h5>
+              <small className="text-muted">
+                  Basic
+              </small>
             </div>
           </div>
+        </div>
+        <div className="col-xs-6">
+          <div className="box p-a">
+            <div className="clear">
+              <h5 className="m-a-0 _500">
+                {this.props.data.EPF}<span className="text-sm"></span>
+              </h5>
+              <small className="text-muted">
+                  EPF
+              </small>
+            </div>
+          </div>
+        </div>
+        <div className="col-xs-6">
+          <div className="box p-a">
+            <div className="clear">
+              <h5 className="m-a-0 _500">
+                {this.props.data.HRA}<span className="text-sm"></span>
+              </h5>
+              <small className="text-muted">
+                  HRA
+              </small>
+            </div>
+          </div>
+        </div>
+        <div className="col-xs-6">
+          <div className="box p-a">
+            <div className="clear">
+              <h5 className="m-a-0 _500">
+                {this.props.data.Loan}<span className="text-sm"></span>
+              </h5>
+              <small className="text-muted">
+                Loan
+              </small>
+            </div>
+          </div>
+        </div>
+        <div className="col-xs-6">
+          <div className="box p-a">
+            <div className="clear">
+              <h5 className="m-a-0 _500">
+                {this.props.data.Conveyance}<span className="text-sm"></span>
+              </h5>
+              <small className="text-muted">
+                Conveyance
+              </small>
+            </div>
+          </div>
+        </div>
+        <div className="col-xs-6">
+          <div className="box p-a">
+            <div className="clear">
+              <h5 className="m-a-0 _500">
+                {this.props.data.Advance}<span className="text-sm"></span>
+              </h5>
+              <small className="text-muted">
+                Advance
+              </small>
+            </div>
+          </div>
+        </div>
+        <div className="col-xs-6">
+          <div className="box p-a">
+            <div className="clear">
+              <h5 className="m-a-0 _500">
+                {this.props.data.Medical_Allowance}<span className="text-sm"></span>
+              </h5>
+              <small className="text-muted">
+                  Medical Allowance
+              </small>
+            </div>
+          </div>
+        </div>
+        <div className="col-xs-6">
+          <div className="box p-a">
+            <div className="clear">
+              <h5 className="m-a-0 _500">
+                  {this.props.data.Misc_Deductions}<span className="text-sm"></span>
+              </h5>
+              <small className="text-muted">
+                  Misc Deductions
+              </small>
+            </div>
+          </div>
+        </div>
+        <div className="col-xs-6">
+          <div className="box p-a">
+            <div className="clear">
+              <h5 className="m-a-0 _500">
+                {this.props.data.Special_Allowance}<span className="text-sm"></span>
+              </h5>
+              <small className="text-muted">
+                  Special Allowance
+              </small>
+            </div>
+          </div>
+        </div>
+        <div className="col-xs-6">
+          <div className="box p-a">
+            <div className="clear">
+              <h5 className="m-a-0 _500">
+                {this.props.data.TDS}<span className="text-sm"></span>
+              </h5>
+              <small className="text-muted">
+                  TDS
+              </small>
+            </div>
+          </div>
+        </div>
+        <div className="col-xs-6">
+          <div className="box p-a">
+            <div className="clear">
+              <h5 className="m-a-0 _500">
+                {this.props.data.Arrears}
+                <span className="text-sm">
+                </span>
+              </h5>
+              <small className="text-muted">
+                  Arrears
+              </small>
+            </div>
+          </div>
+        </div>
+        <div className="col-xs-6">
+          <div className="box p-a">
+            <div className="clear">
+              <div>
+                <small className="text-muted" >{'Holding Ammount'}</small>
+              </div>
+              <TextField
+                id="hold1"
+                fullWidth
+                onChange={(evt) => { this.setState({holdingAmount: evt.target.value}); }}
+                value={this.state.holdingAmount}
+                />
+              <input type="button"
+                id="addHolding" value="Add Holding"
+                className="col-xs-12 md-btn md-raised info button-style-l">
+              </input>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
