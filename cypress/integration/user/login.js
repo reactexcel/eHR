8a7909c5812e54aff45a3b060d29eef8ec690ae2
Ashutosh <arun.etech2011@gmail.com>
2017-12-16 16:50:25 +0530

    formate created for test cases

--- cypress/integration/user/login.js
+++ cypress/integration/user/login.js
@@ -1,7 +1,22 @@
-import { urls, visitIndexRoute, urlVisited, user, signin } from '../../index';
+import { urls, visitIndexRoute, urlVisited, user, signin, signinWithWhitespace, signout } from '../../index';
 
 describe('User login', () => {
   it('user should able to login and redirected to home page', () => {
     signin(user.user);
+    urlVisited(urls.baseUrl+urls.policyDocuments);
+  });
+  it('trim email, so there is no space at the end', () => {
+      signin(user.userWhiteSpace);
+      urlVisited(urls.baseUrl+urls.policyDocuments);
+  });
+  it('user should get promoted for worng password', () => {
+      signin(user.userWrongPassword);
+      cy.get('.sweet-alert').should('be.visible');
+      cy.get('.confirm').click();
+  });
+  it('user should get promoted for worng username with error msg', () => {
+      signin(user.userWrongPassword);
+      cy.get('.sweet-alert').should('be.visible');
+      cy.get('.confirm').click();
   });
 });
