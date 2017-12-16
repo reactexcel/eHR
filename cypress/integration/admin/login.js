c78b11c69865844a8a02774d0eedcef5a153d112
reactexcel <rohit.excel2011@gmail.com>
2017-12-16 11:48:32 +0530

    edit login test

--- cypress/integration/admin/login.js
+++ cypress/integration/admin/login.js
@@ -13,14 +13,14 @@ describe('Test Login Form.', () => {
     cy.get('#loginButton').click();
     urlVisited(urls.baseUrl+urls.login);
   });
-  it('should not go to other page when left blank username/password', () => {
+  it('when enter wrong data show error', () => {
     cy.get('#usernameInput').type('wrongusername');
     cy.get('#passwordInput').type('wrongpassword');
     cy.get('#loginButton').click();
     cy.get('.sweet-alert').wait(3000).should('be.visible');
     cy.get('.confirm').click();
   });
-  it('should not go to other page when left blank username/password', () => {
+  it('login should success', () => {
     cy.get('#usernameInput').clear().type('arun');
     cy.get('#passwordInput').clear().type('java@123');
     cy.get('#loginButton').click().wait(3000);
