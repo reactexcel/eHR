c029162e9a31d86dec7f807284465a179e9a4184
reactexcel <arun.etech2011@gmail.com>
2017-12-18 16:37:35 +0530

    holiday and policy test cases added

---
+++ cypress/integration/generic/holidays.js
@@ -0,0 +1,5 @@
+describe('Test holidays', () => {
+  it('holidays list is not empty', () => {
+
+  });
+});
---
+++ cypress/integration/generic/policydocuments.js
@@ -0,0 +1,5 @@
+describe('Test policy documents', () => {
+  it('policy documents list is not empty', () => {
+
+  });
+});
