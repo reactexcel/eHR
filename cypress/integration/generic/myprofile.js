b299c4f31d6bddfa9ff9526b0cf3f5065c642f5a
reactexcel <arun.etech2011@gmail.com>
2017-12-18 17:40:05 +0530

    my profile test cases added

---
+++ cypress/integration/generic/myprofile.js
@@ -0,0 +1,17 @@
+describe('Test my profile', () => {
+  it('api is fired which will show personal details and bank details information, User Name will be read only', () => {
+
+  });
+	it('api is fired which will show user previous salary slip details', () => {
+
+  });
+  it('on UPDATE PROFILE DETAILS button click Person Details will be updated with new data', () => {
+
+  });
+  it('on UPDATE PROFILE DETAILS button click Bank Details will be updated with new data', () => {
+
+  });
+  it('on UPDATE PASSWORD button click, alert message will be displayed', () => {
+
+  });
+});
--- cypress/integration/generic/policydocuments.js
+++ cypress/integration/generic/policydocuments.js
@@ -2,4 +2,7 @@ describe('Test policy documents', () => {
   it('policy documents list is not empty', () => {
 
   });
+  it('click on any document url will open a new tab, fire an api and status from red change to green or will be green if already green', () => {
+
+  });
 });
