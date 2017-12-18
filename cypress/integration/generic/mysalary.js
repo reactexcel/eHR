b46cea7937b75a88dd8104e3ac206e23454dad40
reactexcel <arun.etech2011@gmail.com>
2017-12-18 18:22:39 +0530

    my salary page test cases

---
+++ cypress/integration/generic/mysalary.js
@@ -0,0 +1,8 @@
+describe('Test policy documents', () => {
+  it('api is fired which will show salary details, salary revision, previous payslips', () => {
+
+  });
+  it('if no salary details, no salary revision, no previous payslips are there then it should be empty', () => {
+
+  });
+});
