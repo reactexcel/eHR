2716c59c0810e4aba5707f7a2b8b841d1ed86593
reactexcel <arun.etech2011@gmail.com>
2017-12-21 11:13:44 +0530

    holiday list test

--- cypress/integration/generic/holidays.js
+++ cypress/integration/generic/holidays.js
@@ -4,17 +4,12 @@ describe('Test holidays', () => {
   it('holidays list is not empty', () => {
     cy.server();
     cy.route({method:'POST',url: apiUrls.apiUrl}).as('login');
-    visitIndexRoute();
-    cy.get('#openLogin').click();
-    urlVisited(urls.baseUrl+urls.login);
     signin(user.admin);
     // cy.request('POST', apiUrls.apiUrl).should('have.property', 'status', 200);
     // cy.wait('@login');
     urlVisited(urls.baseUrl+urls.home);
-  });
-  it('checking holiday list', () => {
     cy.get('#holidays').should('be.visible');
     cy.get('#holidays').click();
-    urlVisited(urls.baseUrl+urls.holidays);  
+    urlVisited(urls.baseUrl+urls.holidays);
   });
 });
--- cypress/integration/generic/policydocuments.js
+++ cypress/integration/generic/policydocuments.js
@@ -1,5 +1,22 @@
+import { urls, apiUrls, visitIndexRoute, urlVisited, user, signin, signinWithWhitespace, signout } from '../../index';
+
 describe('Test policy documents', () => {
   it('policy documents list is not empty', () => {
+    cy.server();
+    cy.route({method:'POST',url: apiUrls.apiUrl}).as('login');
+    signin(user.admin);
+    // cy.request('POST', apiUrls.apiUrl).should('have.property', 'status', 200);
+    cy.wait('@login');
+    urlVisited(urls.baseUrl+urls.home);
+    cy.get('#policy_documents').should('be.visible');
+    cy.get('#policy_documents').click();
+    urlVisited(urls.baseUrl+urls.policyDocuments);
+    cy.get('#SamplePolicy1').should('be.visible');
+    cy.get('#PolicyTest').should('be.visible');
+    cy.get('#test').should('be.visible');
+    cy.get('#testing').should('be.visible');
+  });
+  it('click on any document url will open a new tab, fire an api and status from red change to green or will be green if already green', () => {
 
   });
 });
