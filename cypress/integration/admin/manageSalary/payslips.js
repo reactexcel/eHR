5d4a89ea4c25fa5af2066a5b20bb9f7e7f882d9e
reactexcel <arun.etech2011@gmail.com>
2018-02-02 12:40:34 +0530

    old cypres cases push

---
+++ cypress/integration/admin/manageSalary/payslips.js
@@ -0,0 +1,29 @@
+describe('Test payslips page', () => {
+	it('header title is Manage Payslips', () => {
+
+  });
+  it('list all employees on left panel', () => {
+
+  });
+  if('on page open first employee will be selected on left panel and right panel will be updated with salary details of that employee', () => {
+
+  });
+  if('clicking on an employee on right panel will open his salary details on right panel which will include Generate Payslip, Employee Actual Salary and Previous Payslips', () => {
+
+  });
+  if('', () => {
+
+  });
+  if('', () => {
+
+  });
+  if('', () => {
+
+  });
+  if('', () => {
+
+  });
+  if('', () => {
+
+  });
+});
