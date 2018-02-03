f1b8af04c80760d84cdea74470c1884c0550de93
reactexcel <arun.etech2011@gmail.com>
2018-02-03 15:30:37 +0530

    Test view salary page test case

--- cypress/integration/admin/manageSalary/viewSalary.js
+++ cypress/integration/admin/manageSalary/viewSalary.js
@@ -1,10 +1,19 @@
 import {urls, user, apiUrls} from '../../../index';
-import {signin, signout} from '../../../helper';
+import {signin, signout, apiCall} from '../../../helper';
 import {urlVisited, visitIndexRoute} from '../../../visitRoutes';
 
 describe('Test view salary page', () => {
   it('api will be fired and will list all employees salary', () => {
-
+    visitIndexRoute();
+    signin(user.admin);
+    urlVisited(urls.baseUrl + urls.home);
+    cy.get('.nav > #manage_salary').should('be.visible');
+    cy.get('.nav > #manage_salary').click();
+    cy.get('.nav-sub #view_salary').should('be.visible');
+    cy.get('.nav-sub #view_salary').click();
+    urlVisited(urls.baseUrl + urls.viewSalary);
+    cy.get('.app-body').should('be.visible');
+    cy.get('.app-body .padding').should('be.visible');
   });
   it('search text box will filter the listing', () => {
     visitIndexRoute();
