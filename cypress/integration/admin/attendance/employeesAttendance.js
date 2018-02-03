b11f92f0f0d28273349548f1cd65c3496eb81930
reactexcel <arun.etech2011@gmail.com>
2018-02-03 16:33:30 +0530

    Test employees attendance listing test case

--- cypress/integration/admin/attendance/employeesAttendance.js
+++ cypress/integration/admin/attendance/employeesAttendance.js
@@ -1,8 +1,31 @@
+import {urls, user, apiUrls} from '../../../index';
+import {signin, signout, apiCall} from '../../../helper';
+import {urlVisited, visitIndexRoute} from '../../../visitRoutes';
+
 describe('Test employees attendance listing', () => {
   it('employees list is loaded on left side panel', () => {
-
-  }
+    visitIndexRoute();
+    signin(user.admin);
+    urlVisited(urls.baseUrl + urls.home);
+    cy.get('.nav > #monthly_attendance').should('be.visible');
+    cy.get('.nav > #monthly_attendance').click();
+    cy.get('.nav-sub #home').should('be.visible');
+    cy.get('.nav-sub #home').click();
+    cy.get('.app-body').should('be.visible');
+    cy.get('.app-body .padding').should('be.visible');
+    cy.get('#userList').should('be.visible');
+  });
   it('first employee attendance details are shown on right panel', () => {
-
-  }
+    visitIndexRoute();
+    signin(user.admin);
+    urlVisited(urls.baseUrl + urls.home);
+    cy.get('.nav > #monthly_attendance').should('be.visible');
+    cy.get('.nav > #monthly_attendance').click();
+    cy.get('.nav-sub #home').should('be.visible');
+    cy.get('.nav-sub #home').click();
+    cy.get('.app-body').should('be.visible');
+    cy.get('.app-body .padding').should('be.visible');
+    cy.get('#content').should('be.visible');
+    cy.get('#content #calendar').should('be.visible');
+  });
 });
