c433a30ef0f5454dcbd7c95ecc284f4a77f93fe4
reactexcel <arun.etech2011@gmail.com>
2018-02-03 16:40:22 +0530

    Test attendance upload page test case

--- cypress/integration/admin/attendance/uploadAttendance.js
+++ cypress/integration/admin/attendance/uploadAttendance.js
@@ -1,5 +1,16 @@
+import {urls, user, apiUrls} from '../../../index';
+import {signin, signout, apiCall} from '../../../helper';
+import {urlVisited, visitIndexRoute} from '../../../visitRoutes';
+
 describe('Test attendance upload page', () => {
   it('page load successfully', () => {
-
-  }
+    visitIndexRoute();
+    signin(user.admin);
+    urlVisited(urls.baseUrl + urls.home);
+    cy.get('.nav > #monthly_attendance').should('be.visible');
+    cy.get('.nav > #monthly_attendance').click();
+    cy.get('.nav-sub #uploadAttendance').should('be.visible');
+    cy.get('.nav-sub #uploadAttendance').click();
+    urlVisited(urls.baseUrl + urls.uploadAttendance);
+  });
 });
