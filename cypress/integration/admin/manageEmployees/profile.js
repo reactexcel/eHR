64b26a61fba471ce71ee1a04b74bea77a93b0455
reactexcel <arun.etech2011@gmail.com>
2018-02-03 17:54:01 +0530

    Test profile page test case

--- cypress/integration/admin/manageEmployees/profile.js
+++ cypress/integration/admin/manageEmployees/profile.js
@@ -1,11 +1,25 @@
+import {urls, user, apiUrls} from '../../../index';
+import {signin, signout, apiCall} from '../../../helper';
+import {urlVisited, visitIndexRoute} from '../../../visitRoutes';
+
 describe('Test profile page', () => {
   it('employees list is loaded on left side panel', () => {
-
-  }
+    visitIndexRoute();
+    signin(user.admin);
+    urlVisited(urls.baseUrl + urls.home);
+    cy.get('.nav > #manage_users').should('be.visible');
+    cy.get('.nav > #manage_users').click();
+    cy.get('.nav-sub #manage_users').should('be.visible');
+    cy.get('.nav-sub #manage_users').click();
+    urlVisited(urls.baseUrl + urls.manageUsers);
+    cy.get('.app-body').should('be.visible');
+    cy.get('.app-body .padding').should('be.visible');
+    cy.get(' #userList').should('be.visible');
+  });
   it('first employee profile details are shown on right panel', () => {
 
-  }
+  });
   it('add new employee form opens when click on Add New Employee button', () => {
 
-  }
+  });
 });
--- cypress/integration/admin/manageSalary/salaries.js
+++ cypress/integration/admin/manageSalary/salaries.js
@@ -1,14 +1,27 @@
+import {urls, user, apiUrls} from '../../../index';
+import {signin, signout, apiCall} from '../../../helper';
+import {urlVisited, visitIndexRoute} from '../../../visitRoutes';
+
 describe('Test salaries page', () => {
   it('empoyees list will be visible on left side panel', () => {
-
-  }
+    visitIndexRoute();
+    signin(user.admin);
+    urlVisited(urls.baseUrl + urls.home);
+    cy.get('.nav > #manage_salary').should('be.visible');
+    cy.get('.nav > #manage_salary').click();
+    cy.get('.nav-sub #manage_salary').should('be.visible');
+    cy.get('.nav-sub #manage_salary').click();
+    urlVisited(urls.baseUrl + urls.manageSalary);
+    cy.get('.app-body').should('be.visible');
+    cy.get('.app-body .padding').should('be.visible');
+  });
   it('click on any employee will show his salary details on right side panel', () => {
 
-  }
+  });
   it('when new salary is added from 2nd column it will list in the first column', () => {
 
-  }
+  });
   it('when new holding is added from 3rd column it will list in the same column', () => {
 
-  }
+  });
 });
