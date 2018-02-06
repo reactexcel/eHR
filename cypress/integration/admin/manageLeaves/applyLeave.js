58c3503668d341c26bcd33f32f551fab3ab50f57
reactexcel <arun.etech2011@gmail.com>
2018-02-06 17:42:50 +0530

    applyleave test

--- cypress/integration/admin/manageLeaves/applyLeave.js
+++ cypress/integration/admin/manageLeaves/applyLeave.js
@@ -1,8 +1,29 @@
+import {urls, user, apiUrls, monthName} from '../../../index';
+import {signin, signout, apiCall} from '../../../helper';
+import {urlVisited, visitIndexRoute} from '../../../visitRoutes';
+
 describe('Test apply leave page', () => {
   it('left panel will have all users list', () => {
-
-  }
+    visitIndexRoute();
+    signin(user.admin);
+    urlVisited(urls.baseUrl + urls.home);
+    cy.get('.nav > #manage_leaves').should('be.visible');
+    cy.get('.nav > #manage_leaves').click();
+    cy.get('.nav-sub > #manage_leaves').should('be.visible');
+    cy.get('.nav-sub > #manage_leaves').click();
+    cy.get('.app-body').should('be.visible');
+    cy.get('.app-body .padding').should('be.visible');
+    cy.get('#userListWrapper').should('be.visible');
+  });
   it('clicking on any user on left panel and filling information on right side, click on apply button will apply leave', () => {
-
-  }
+    visitIndexRoute();
+    signin(user.admin);
+    urlVisited(urls.baseUrl + urls.home);
+    cy.get('.nav > #manage_leaves').should('be.visible');
+    cy.get('.nav > #manage_leaves').click();
+    cy.get('.nav-sub > #manage_leaves').should('be.visible');
+    cy.get('.nav-sub > #manage_leaves').click();
+    cy.get('.app-body').should('be.visible');
+    cy.get('.app-body .padding').should('be.visible');
+  });
 });
