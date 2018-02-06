b2c151e09294d4769ace71959790f4d1c3aaa2a9
reactexcel <arun.etech2011@gmail.com>
2018-02-06 18:57:53 +0530

    leave page test

--- cypress/integration/admin/manageLeaves/applyLeave.js
+++ cypress/integration/admin/manageLeaves/applyLeave.js
@@ -9,11 +9,10 @@ describe('Test apply leave page', () => {
     urlVisited(urls.baseUrl + urls.home);
     cy.get('.nav > #manage_leaves').should('be.visible');
     cy.get('.nav > #manage_leaves').click();
-    cy.get('.nav-sub > #manage_leaves').should('be.visible');
-    cy.get('.nav-sub > #manage_leaves').click();
+    cy.get('.nav-sub > #apply_leave').should('be.visible');
+    cy.get('.nav-sub > #apply_leave').click();
     cy.get('.app-body').should('be.visible');
     cy.get('.app-body .padding').should('be.visible');
-    cy.get('#userListWrapper').should('be.visible');
   });
   it('clicking on any user on left panel and filling information on right side, click on apply button will apply leave', () => {
     visitIndexRoute();
@@ -21,8 +20,8 @@ describe('Test apply leave page', () => {
     urlVisited(urls.baseUrl + urls.home);
     cy.get('.nav > #manage_leaves').should('be.visible');
     cy.get('.nav > #manage_leaves').click();
-    cy.get('.nav-sub > #manage_leaves').should('be.visible');
-    cy.get('.nav-sub > #manage_leaves').click();
+    cy.get('.nav-sub > #apply_leave').should('be.visible');
+    cy.get('.nav-sub > #apply_leave').click();
     cy.get('.app-body').should('be.visible');
     cy.get('.app-body .padding').should('be.visible');
   });
--- cypress/integration/admin/manageLeaves/leaves.js
+++ cypress/integration/admin/manageLeaves/leaves.js
@@ -1,11 +1,33 @@
+import {urls, user, apiUrls, monthName} from '../../../index';
+import {signin, signout, apiCall} from '../../../helper';
+import {urlVisited, visitIndexRoute} from '../../../visitRoutes';
+
 describe('Test leaves page', () => {
   it('api is fired to get leaves and will list under the 4 tabs', () => {
-
-  }
+    visitIndexRoute();
+    signin(user.admin);
+    urlVisited(urls.baseUrl + urls.home);
+    cy.get('.nav > #manage_leaves').should('be.visible');
+    cy.get('.nav > #manage_leaves').click();
+    cy.get('.nav-sub > #manage_leaves').should('be.visible');
+    cy.get('.nav-sub > #manage_leaves').click();
+    cy.get('.app-body').should('be.visible');
+    cy.get('.app-body .padding').should('be.visible');
+    cy.get('.app-body .padding .row > div').should('be.visible');
+  });
   it('every tab will have list of employees falls under that tab in left panel', () => {
-
-  }
+    visitIndexRoute();
+    signin(user.admin);
+    urlVisited(urls.baseUrl + urls.home);
+    cy.get('.nav > #manage_leaves').should('be.visible');
+    cy.get('.nav > #manage_leaves').click();
+    cy.get('.nav-sub > #manage_leaves').should('be.visible');
+    cy.get('.nav-sub > #manage_leaves').click();
+    cy.get('.app-body').should('be.visible');
+    cy.get('.app-body .padding').should('be.visible');
+    cy.get('.app-body .padding .row > div').should('be.visible');
+  });
   it('clicking any user on left panel will show respective leave detail on right panel', () => {
 
-  }
+  });
 });
