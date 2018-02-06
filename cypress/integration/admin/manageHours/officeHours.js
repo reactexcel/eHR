f2df3a3bd19d52efd8457dd0c9c2b259f247e77a
reactexcel <arun.etech2011@gmail.com>
2018-02-06 16:58:32 +0530

    office hour and pending hour test

--- cypress/integration/admin/manageHours/officeHours.js
+++ cypress/integration/admin/manageHours/officeHours.js
@@ -35,10 +35,31 @@ describe('Test office hours', () => {
   it('clicking on next and previous month will show the respective month data, month name and year text will also change', () => {
 
   });
-  if ('clicking on Change To text box of any date will open a select box listing time', () => {
-
+  it('clicking on Change To text box of any date will open a select box listing time', () => {
+    visitIndexRoute();
+    signin(user.admin);
+    urlVisited(urls.baseUrl + urls.home);
+    cy.get('.nav > #manage_working_hours').should('be.visible');
+    cy.get('.nav > #manage_working_hours').click();
+    cy.get('.nav-sub #manage_working_hours').should('be.visible');
+    cy.get('.nav-sub #manage_working_hours').click();
+    urlVisited(urls.baseUrl + urls.manageWorkingHours);
+    cy.get('.app-body').should('be.visible');
+    cy.get('.app-body .padding').should('be.visible');
+    cy.get('#content').should('be.visible');
+    cy.get('#calendar').should('be.visible');
+    cy.get('#calendar .fc-event-container .fc-content').should('be.visible');
+    cy.get('#calendar .fc-event-container .fc-content .fc-time .timepicker').should('be.visible');
+    const data = [];
+    cy.get('#calendarDay').each((item, index, list) => {
+      cy.get('#calendarDay:nth-child(' + (index + 1) + ') > div').then((id) => {
+        data.push(id.attr('id'));
+        console.log(data, 'asd');
+      });
+    });
+    // cy.get('#calendar .fc-event-container .fc-content .fc-time input').click({multiple: true, force: true}).get('.ui-timepicker-wrapper').should('be.visible');
   });
-  if ('on selecting time for any date text box value will be the opted time and Hours will also change to the opted value', () => {
+  it('on selecting time for any date text box value will be the opted time and Hours will also change to the opted value', () => {
 
   });
 });
--- cypress/integration/admin/manageHours/pendingHours.js
+++ cypress/integration/admin/manageHours/pendingHours.js
@@ -1,8 +1,24 @@
-describe('Test office hours', () => {
-	it('header title is Manage Employee Pending Hours', () => {
+import {urls, user, apiUrls, monthName} from '../../../index';
+import {signin, signout, apiCall} from '../../../helper';
+import {urlVisited, visitIndexRoute} from '../../../visitRoutes';
 
+describe('Test office hours', () => {
+  it('header title is Manage Employee Pending Hours', () => {
+    cy.server();
+    visitIndexRoute();
+    signin(user.admin);
+    urlVisited(urls.baseUrl + urls.home);
+    cy.route(apiUrls.apiUrl).then((response) => {
+      expect(response.status).to.eq(200);
+    });
+    cy.get('.nav > #manage_working_hours').should('be.visible');
+    cy.get('.nav > #manage_working_hours').click();
+    cy.get('.nav-sub #manage_user_pending_hours').should('be.visible');
+    cy.get(' #manage_user_pending_hours').click();
+    urlVisited(urls.baseUrl + urls.manageUserPendingHours);
+    cy.get('.navbar .navbar-item').contains('Manage Employee Pending Hours');
   });
-	it('is listing current month pending hours, if no data will show Not Updated! ', () => {
+  it('is listing current month pending hours, if no data will show Not Updated! ', () => {
 
   });
   it('clicking on next and previous month will show the respective month data, month name and year text will also change', () => {
