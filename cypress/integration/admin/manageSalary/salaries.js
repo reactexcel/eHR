c92f0353365e56eac297a3ac32b55221ee99d83f
reactexcel <arun.etech2011@gmail.com>
2018-02-17 16:17:18 +0530

    salary test error resolved

--- cypress/integration/admin/manageSalary/salaries.js
+++ cypress/integration/admin/manageSalary/salaries.js
@@ -3,8 +3,9 @@ import {signin, signout, apiCall} from '../../../helper';
 import {urlVisited, visitIndexRoute} from '../../../visitRoutes';
 
 describe('Test salaries page', () => {
-  xit('empoyees list will be visible on left side panel', () => {
+  it('empoyees list will be visible on left side panel', () => {
     signin(user.admin);
+    cy.wait(4000);
     urlVisited(urls.baseUrl + urls.home);
     cy.get('.nav > #manage_salary').should('be.visible');
     cy.get('.nav > #manage_salary').click();
@@ -14,13 +15,40 @@ describe('Test salaries page', () => {
     cy.get('.app-body').should('be.visible');
     cy.get('.app-body .padding').should('be.visible');
   });
-  xit('click on any employee will show his salary details on right side panel', () => {
-
+  it('click on any employee will show his salary details on right side panel', () => {
+    signin(user.admin);
+    cy.wait(4000);
+    urlVisited(urls.baseUrl + urls.home);
+    cy.get('.nav > #manage_salary').should('be.visible');
+    cy.get('.nav > #manage_salary').click();
+    cy.get('.nav-sub #manage_salary').should('be.visible');
+    cy.get('.nav-sub #manage_salary').click();
+    urlVisited(urls.baseUrl + urls.manageSalary);
+    cy.get('.app-body').should('be.visible');
+    cy.get('.app-body .padding').should('be.visible');
   });
-  xit('when new salary is added from 2nd column it will list in the first column', () => {
-
+  it('when new salary is added from 2nd column it will list in the first column', () => {
+    signin(user.admin);
+    cy.wait(4000);
+    urlVisited(urls.baseUrl + urls.home);
+    cy.get('.nav > #manage_salary').should('be.visible');
+    cy.get('.nav > #manage_salary').click();
+    cy.get('.nav-sub #manage_salary').should('be.visible');
+    cy.get('.nav-sub #manage_salary').click();
+    urlVisited(urls.baseUrl + urls.manageSalary);
+    cy.get('.app-body').should('be.visible');
+    cy.get('.app-body .padding').should('be.visible');
   });
-  xit('when new holding is added from 3rd column it will list in the same column', () => {
-
+  it('when new holding is added from 3rd column it will list in the same column', () => {
+    signin(user.admin);
+    cy.wait(4000);
+    urlVisited(urls.baseUrl + urls.home);
+    cy.get('.nav > #manage_salary').should('be.visible');
+    cy.get('.nav > #manage_salary').click();
+    cy.get('.nav-sub #manage_salary').should('be.visible');
+    cy.get('.nav-sub #manage_salary').click();
+    urlVisited(urls.baseUrl + urls.manageSalary);
+    cy.get('.app-body').should('be.visible');
+    cy.get('.app-body .padding').should('be.visible');
   });
 });
