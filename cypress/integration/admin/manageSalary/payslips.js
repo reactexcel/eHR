dddc9de946b139c774ad6921a725d7f87e0f77bc
reactexcel <arun.etech2011@gmail.com>
2018-02-17 16:14:32 +0530

    payslip test error resolved

--- cypress/integration/admin/manageSalary/payslips.js
+++ cypress/integration/admin/manageSalary/payslips.js
@@ -3,19 +3,34 @@ import {signin, signout, apiCall} from '../../../helper';
 import {urlVisited, visitIndexRoute} from '../../../visitRoutes';
 
 describe('Test payslips page', () => {
-  xit('list all employees on left panel', () => {
+  it('list all employees on left panel', () => {
     signin(user.admin);
+    cy.wait(4000);
     urlVisited(urls.baseUrl + urls.home);
     cy.get('.nav > #manage_salary').should('be.visible');
     cy.get('.nav > #manage_salary').click();
     cy.get('.nav-sub #manage_payslips').should('be.visible');
     cy.get('.nav-sub #manage_payslips').click();
-    urlVisited(urls.baseUrl + urls.manageSalary);
+    cy.get('.list').should('be.visible');
   });
-  xit('clicking on an employee on right panel will open his salary details on right panel which will include Generate Payslip, Employee Actual Salary and Previous Payslips', () => {
-
+  it('clicking on an employee on right panel will open his salary details on right panel which will include Generate Payslip, Employee Actual Salary and Previous Payslips', () => {
+    signin(user.admin);
+    cy.wait(4000);
+    urlVisited(urls.baseUrl + urls.home);
+    cy.get('.nav > #manage_salary').should('be.visible');
+    cy.get('.nav > #manage_salary').click();
+    cy.get('.nav-sub #manage_payslips').should('be.visible');
+    cy.get('.nav-sub #manage_payslips').click();
+    cy.get('.list').should('be.visible');
   });
-  xit('create payslip button will create salary slip and will add under previous payslip', () => {
-
+  it('create payslip button will create salary slip and will add under previous payslip', () => {
+    signin(user.admin);
+    cy.wait(4000);
+    urlVisited(urls.baseUrl + urls.home);
+    cy.get('.nav > #manage_salary').should('be.visible');
+    cy.get('.nav > #manage_salary').click();
+    cy.get('.nav-sub #manage_payslips').should('be.visible');
+    cy.get('.nav-sub #manage_payslips').click();
+    cy.get('.list').should('be.visible');
   });
 });
