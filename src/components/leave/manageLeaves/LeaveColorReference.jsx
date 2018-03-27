06221c50b10a215d8e807c36e65c7f4ffbff6a5e
excellencetechnologies <excellencetechnologies08@gmail.com>
2018-03-27 17:27:05 +0530

    removed hrapproval

--- src/components/leave/manageLeaves/LeaveColorReference.jsx
+++ src/components/leave/manageLeaves/LeaveColorReference.jsx
@@ -6,28 +6,28 @@ const LeaveColorReference = ({filterLeaveList, selectedTab, userRole}) => {
   if (userRole === CONFIG.ADMIN) {
     return (
       <div className="row no-gutter m-b text-xs l-h-1x" id="manage_leave_header">
-        <div className="col-xs-6 col-sm-3 text-center leaves-tab" onClick={() => filterLeaveList('ApprovedByHr')} >
+        {/* <div className="col-xs-6 col-sm-3 text-center leaves-tab" onClick={() => filterLeaveList('ApprovedByHr')} >
           <div className={'top-bar ' + (selectedTab === 'ApprovedByHr' ? 'active-tab' : '')}></div>
           <div className="p-a blue">
             <h4></h4>
             <div className="text-u-c _600 text-sm" >Approved By HR</div>
           </div>
-        </div>
-        <div className="col-xs-6 col-sm-3 text-center leaves-tab" onClick={() => filterLeaveList('NotApprovedByHr')} >
-          <div className={'top-bar ' + (selectedTab === 'NotApprovedByHr' ? 'active-tab' : '')} ></div>
+        </div> */}
+        <div className="col-xs-6 col-sm-4 text-center leaves-tab" onClick={() => filterLeaveList('PendingAdmin')} >
+          <div className={'top-bar ' + (selectedTab === 'PendingAdmin' ? 'active-tab' : '')} ></div>
           <div className="p-a yellow-A200">
             <h4></h4>
-            <div className=" text-u-c _600 text-sm" >Not Approved By HR</div>
+            <div className=" text-u-c _600 text-sm" >{/*Not Approved By HR*/}Pending</div>
           </div>
         </div>
-        <div className="col-xs-6 col-sm-3 text-center leaves-tab" onClick={() => filterLeaveList('Approved')} >
+        <div className="col-xs-6 col-sm-4 text-center leaves-tab" onClick={() => filterLeaveList('Approved')} >
           <div className={'top-bar ' + (selectedTab === 'Approved' ? 'active-tab' : '')} ></div>
           <div className="p-a green-A200">
             <h4></h4>
             <div className=" text-u-c _600 text-sm" >Approved</div>
           </div>
         </div>
-        <div className="col-xs-6 col-sm-3 text-center leaves-tab" onClick={() => filterLeaveList('Rejected')} >
+        <div className="col-xs-6 col-sm-4 text-center leaves-tab" onClick={() => filterLeaveList('Rejected')} >
           <div className={'top-bar ' + (selectedTab === 'Rejected' ? 'active-tab' : '')} ></div>
           <div className="p-a red-500">
             <h4></h4>
