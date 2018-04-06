501e68075736d0fdd9bb32ecb9df5b8ebdf64d4a
excellencetechnologies <excellencetechnologies08@gmail.com>
2018-04-06 11:02:13 +0530

    removed minor error which was leading to  no list on disabled employee page

--- src/components/generic/UsersList.jsx
+++ src/components/generic/UsersList.jsx
@@ -31,7 +31,7 @@ const UsersList = ({users, selectedUserId, onUserClick, disabledUser, header, to
       avatar = <img src={profileImae} />;
       param = user.user_Id;
     } else {
-      profileImae = user.name.charAt(0);
+      profileImae = user.username.charAt(0);
       avatar = <Avatar>{profileImae}</Avatar>;
       param = user;
     }
