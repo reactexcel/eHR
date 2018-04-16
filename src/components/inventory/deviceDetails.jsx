7df6b2281fc87ddde6e0d5ebc184a6d15b5a09f2
excellencetechnologies <excellencetechnologies08@gmail.com>
2018-04-16 15:35:45 +0530

    Added id in device details table

--- src/components/inventory/deviceDetails.jsx
+++ src/components/inventory/deviceDetails.jsx
@@ -1,22 +1,40 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import _ from 'lodash';
-import 'react-date-picker/index.css';
+import React from "react";
+import PropTypes from "prop-types";
+import _ from "lodash";
+import "react-date-picker/index.css";
 
-const DeviceDetails = ({userAssignMachine, unassignDevice}) => {
- 
+const DeviceDetails = ({ userAssignMachine, unassignDevice }) => {
   let machineList = _.map(userAssignMachine, (val, i) => {
-    
-    return (<tr key={i}>
-      <td>{val.machine_type}</td>
-      <td>{val.machine_name}</td>
-      <td>{val.serial_number}</td>
-      <td>{val.assign_date}</td>
-      <td style={{textAlign:"center"}}><i className="fa fa-lg fa fa-trash" onClick={() => unassignDevice(val)} aria-hidden="true" style={{color: "rgb(183, 28, 28)",textAlign:"center", cursor: "pointer"}}></i></td>
-    </tr>);
+    return (
+      <tr key={i}>
+        <td>{val.id}</td>
+        <td>{val.machine_type}</td>
+        <td>{val.machine_name}</td>
+        <td>{val.serial_number}</td>
+        <td>{val.assign_date}</td>
+        <td style={{ textAlign: "center" }}>
+          <i
+            className="fa fa-lg fa fa-trash"
+            onClick={() => unassignDevice(val)}
+            aria-hidden="true"
+            style={{
+              color: "rgb(183, 28, 28)",
+              textAlign: "center",
+              cursor: "pointer"
+            }}
+          />
+        </td>
+      </tr>
+    );
   });
   if (_.isEmpty(machineList)) {
-    machineList = <tr><td colSpan="5"><h5 className="text-center text-danger">{'Device Not Asssigned'}</h5></td></tr>;
+    machineList = (
+      <tr>
+        <td colSpan="5">
+          <h5 className="text-center text-danger">{"Device Not Asssigned"}</h5>
+        </td>
+      </tr>
+    );
   }
 
   return (
@@ -26,16 +44,15 @@ const DeviceDetails = ({userAssignMachine, unassignDevice}) => {
         <table className="table table-striped table-hover">
           <thead>
             <tr>
+              <th>ID</th>
               <th>Device Type</th>
               <th>Name</th>
               <th>Serial Number</th>
               <th>Assign Date</th>
-              <th style={{textAlign:"center"}}>Unassign</th>
+              <th style={{ textAlign: "center" }}>Unassign</th>
             </tr>
           </thead>
-          <tbody>
-            {machineList}
-          </tbody>
+          <tbody>{machineList}</tbody>
         </table>
       </div>
     </div>
