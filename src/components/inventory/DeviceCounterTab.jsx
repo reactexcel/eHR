b33ce847924bc0137ffa76bdfbe88ac68d68be97
reactexcel <arun.etech2011@gmail.com>
2018-07-28 14:33:53 +0530

    download csv file

--- src/components/inventory/DeviceCounterTab.jsx
+++ src/components/inventory/DeviceCounterTab.jsx
@@ -3,25 +3,65 @@ import React from "react";
 import PropTypes from "prop-types";
 import DeviceCounter from "./DeviceCounter";
 import style from "src/styles/inventory/viewUser.scss";
+import {CSVLink} from 'react-csv';
 //  import FormAddNewInventory from '/home/etech/Documents/ReactReduxHR/src/modules/inventory/components/AddInventory.jsx'
+
+
 
 const DeviceCounterTab = ({ statusList, deviceCountList, router }) => {
   let total = 0;
   let newDeviceCountData = [];
+  let headers = [
+    {label: 'Device Name', key: 'deviceName'},
+  ];
+  let datas =[];
+  let headerData=[];
+  Object.keys(deviceCountList).map((key)=>{
+    Object.keys(deviceCountList[key]).map((keys)=>{
+      headers.map((data)=>{
+        if(data.label !== keys){
+            headerData.push({
+            label:keys,
+            key:keys
+          })
+        }
+      })
+    });
+  });
+  let hdata =  _.map(
+    _.uniq(
+        _.map(headerData, function(obj){
+            return JSON.stringify(obj);
+        })
+    ), function(obj) {
+        return JSON.parse(obj);
+    }
+  );
+    let headersData = headers.concat(hdata);
+    Object.keys(deviceCountList).map((key)=>{
+      let deriveData = {
+        deviceName:key,
+      }
+      Object.keys(deviceCountList[key]).map((keys)=>{
+           deriveData[keys] = deviceCountList[key][keys]
+      });
+      datas.push(deriveData);
+    });
+
   for (var key in deviceCountList) {
     if (deviceCountList.hasOwnProperty(key)) {
       let deviceData = deviceCountList[key];
       total = total + deviceData.total;
       newDeviceCountData.push(
         <DeviceCounter
-          key={key}
+        key={key}
           deviceData={deviceData}
           deviceName={key}
           router={router}
-        />
-      );
+          />
+        );
+      }
     }
-  }
   let statusList1 = statusList.map((val, i) => {
     return (
       <div className="col-xs-4 col-sm-4 col-md-3 col-lg-2" key={i}>
@@ -56,6 +96,9 @@ const DeviceCounterTab = ({ statusList, deviceCountList, router }) => {
             <div className="clear">
               <h4 className="m-a-0 text-lg _300" />
               <small className="text-muted"> Total Device </small>
+              <CSVLink data={datas} headers={headersData} filename={"device-report.csv"}  className="btn btn-success" style={{position: "absolute",right:"2%",bottom:'50%'}} >
+                  Download Device Report
+              </CSVLink>
               <h1 style={{ float: "right", color: "#0E9BB1" }}>{total}</h1>
             </div>
           </div>
