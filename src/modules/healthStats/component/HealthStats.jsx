be514bd7e1cd9729509df586e29478acc0078caf
reactexcel <arun.etech2011@gmail.com>
2018-09-19 14:40:18 +0530

    displaying data as response

--- src/modules/healthStats/component/HealthStats.jsx
+++ src/modules/healthStats/component/HealthStats.jsx
@@ -4,11 +4,11 @@ export default class HealthStats extends React.Component {
     render() {
         let data;
         if (this.props.attendance_rows) {
-            data = Object.keys(this.props.attendance_rows).map((item, index) => {
+            data = this.props.attendance_rows.map((item, index) => {
                 return (
                     <tr key={index}>
-                        <td>{item}</td>
-                        <td>{this.props.attendance_rows[`${item}`]}</td>
+                        <td>{item.year}</td>
+                        <td>{item.count}</td>
                     </tr>
                 );
             });
@@ -16,20 +16,20 @@ export default class HealthStats extends React.Component {
         return (
             <div className="row" style={{margin: "0px"}}>
                 <div className="col-sm-4" style={{backgroundColor: "white"}}>
-                        <h5 style={{ padding: "10px" }}>Database Health - Attendance Table</h5>
-                            <table className="table">
-                                <thead>
-                                    <tr>
-                                        <th>Year</th>
-                                        <th>Count</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    {data}
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
+                    <h5 style={{ padding: "10px" }}>Database Health - Attendance Table</h5>
+                    <table className="table">
+                        <thead>
+                            <tr>
+                                <th>Year</th>
+                                <th>Count</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {data}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         )
     }
 }
