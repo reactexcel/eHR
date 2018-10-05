10e7b45f5edd65fc92abdb6bb5dc1da1b954b90d
reactexcel <arun.etech2011@gmail.com>
2018-10-05 11:23:20 +0530

    salary payslip block

--- src/components/salary/userSalary/PayslipHistory.jsx
+++ src/components/salary/userSalary/PayslipHistory.jsx
@@ -1,35 +1,92 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import _ from 'lodash';
+import { dateFormatter } from "src/helper/helper";
 
-const PayslipHistory = ({payslip_history}) => {
+const PayslipHistory = ({ payslip_history }) => {
+  console.log(payslip_history);
+  
   let previousPayslips = _.map(payslip_history, (d, key) => {
     return (
-      <div className="sl-item b-info" key={key} >
-        <div className="sl-content">
-          <div className="sl-date text-muted"><b>{d.month}</b></div>
-          <div className="sl-date text-muted"><b>{d.total_leave_taken}</b> : Total Leave Taken</div>
-          <div className="sl-date text-muted"><b>{d.leave_balance}</b> : Leave Balance</div>
-          <div className="sl-date text-muted"><b>{d.allocated_leaves} : Allocated Leaves</b></div>
-          <div className="sl-date text-muted"><b>{d.paid_leaves} : Paid Leaves</b></div>
-          <div className="sl-date text-muted"><b>{d.unpaid_leaves} : Unpaid Leaves</b></div>
-          <div className="sl-date text-muted"><b>{d.final_leave_balance} : Final Leave Balance</b></div>
-          <div className="sl-date text-muted">
-            {parseInt(d.status) !== 0 ? <a href={`${d.payslip_url}`} target="_BLANK">View Payslip</a> : ''}
+      <tr key={key}>
+        <td>
+          {dateFormatter().fullMonths[parseInt(d.month) - 1]} , {d.year}
+        </td>
+        <td>{d.total_leave_taken}</td>
+        <td>{d.leave_balance}</td>
+        <td>{d.allocated_leaves}</td>
+        <td>{d.paid_leaves}</td>
+        <td>{d.unpaid_leaves}</td>
+        <td>{d.final_leave_balance}</td>
+        <td>
+          {parseInt(d.status) !== 0 ? <a href={`${d.payslip_url}`} target="_BLANK">View Payslip</a> : ''}
+        </td>
+      </tr >
+  /* <div className="" key={key} >
+        <div className="col-sm-12">
+          <div className="col-sm-1">
+          <div className="col-sm-12">
+            {dateFormatter().fullMonths[parseInt(d.month) - 1]} , {d.year}</div>
           </div>
-        </div>
-      </div>
-    );
-  });
+          <div className="col-sm-1">
+            <div className="col-sm-12">{d.total_leave_taken}</div>
+          </div>
+          <div className="col-sm-2">
+            <div className="col-sm-12">{d.leave_balance}</div>
+          </div>
+          <div className="col-sm-2">
+            <div className="col-sm-12">{d.allocated_leaves}</div>
+          </div>
+          <div className="col-sm-2">
+            <div className="col-sm-12">{d.paid_leaves}</div>
+          </div>
+          <div className="col-sm-2">
+            <div className="col-sm-12">{d.unpaid_leaves}</div>
+          </div>
+          <div className="col-sm-2">
+            <div className="col-sm-12">{d.final_leave_balance}</div>
+          </div>
+            <div className="col-sm-12">
+              {parseInt(d.status) !== 0 ? <a href={`${d.payslip_url}`} target="_BLANK">View Payslip</a> : ''}
+            </div>
+          </div>
+        </div> */
+        );
+});
 
-  return (
-    <div>
-      <div className="streamline b-l m-l">
+return (
+  <div>
+    <table className="table table-responsive" style={{overflow: "auto", overflowY: "hidden" }}>
+      <thead>
+        <tr>
+          <td>Month</td>
+          <td>Total Leave Taken</td>
+          <td>Leave Balance</td>
+          <td>Allocated Leaves</td>
+          <td>Paid Leaves</td>
+          <td>Unpaid Leaves</td>
+          <td>Final Leave Balance</td>
+        </tr>
+      </thead>
+      <tbody>
         {previousPayslips}
-      </div>
-    </div>
-  );
-};
+      </tbody>
+    </table>
+    {/* <div className="row">
+            <b>
+              <div className="col-sm-1">Month</div>
+              <div className="col-sm-1">Total Leave Taken</div>
+              <div className="col-sm-2">Leave Balance</div>
+              <div className="col-sm-2">Allocated Leaves</div>
+              <div className="col-sm-2">Paid Leaves</div>
+              <div className="col-sm-2">Unpaid Leaves</div>
+              <div className="col-sm-2">Final Leave Balance</div>
+            </b>
+            {previousPayslips}
+          </div> */}
+  </div>
+);
+      };
 
 PayslipHistory.PropTypes = {
   payslip_history: PropTypes.array.isRequired
