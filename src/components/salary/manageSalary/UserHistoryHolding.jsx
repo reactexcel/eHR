0a994ca02c08af7b0cfa5990d8b78c2949012a93
reactexcel <arun.etech2011@gmail.com>
2018-10-31 15:43:22 +0530

    holding table issue

--- src/components/salary/manageSalary/UserHistoryHolding.jsx
+++ src/components/salary/manageSalary/UserHistoryHolding.jsx
@@ -4,22 +4,23 @@ import PropTypes from 'prop-types';
 import moment from 'moment';
 
 const UserHistoryHolding = ({ data }) => {
-    let holdingHistory = _.map(data, (item, key) => {
-        return (
-            <tr key={key}>
-                <td>Rs.{item.holding_amt}</td>
-                <td>{moment(item.holding_start_date).format("DD-MMMM-YYYY")}</td>
-                <td>{moment(item.holding_end_date).format("DD-MMMM-YYYY")} ({item.holding_month} {item.holding_month < 2 ? 'month' : 'months' })</td>
-                <td>{item.reason}</td>
-                <td>{moment(item.last_updated_on).format("DD-MMMM-YYYY")}</td>
-            </tr>
-        )
-    });
-    if(data.length == 0){
-        return(
-            <div className="text-center">No holding data available</div>
-        )
+    let holdingHistory = (<tr>
+        <td colSpan={5}><div className="text-center">No holding data available</div></td>
+    </tr>)
+    if (data.length) {
+        holdingHistory = _.map(data, (item, key) => {
+            return (
+                <tr key={key}>
+                    <td>Rs.{item.holding_amt}</td>
+                    <td>{moment(item.holding_start_date).format("DD-MMMM-YYYY")}</td>
+                    <td>{moment(item.holding_end_date).format("DD-MMMM-YYYY")} ({item.holding_month} {item.holding_month < 2 ? 'month' : 'months'})</td>
+                    <td>{item.reason}</td>
+                    <td>{moment(item.last_updated_on).format("DD-MMMM-YYYY")}</td>
+                </tr>
+            )
+        });
     }
+
     return (
         <div>
             <table className="table table-striped">
