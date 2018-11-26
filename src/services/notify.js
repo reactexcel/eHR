80397b6ca7e7b31d6c8ffbdda6e5f8651fedf5aa
rohit kumar <rkumar@netchexonline.com>
2018-11-26 12:05:55 +0530

    salary added modal is done

--- src/services/notify.js
+++ src/services/notify.js
@@ -3,16 +3,18 @@ export function notify (title, text, type) {
   sweetAlert(title, text, type);
 }
 
-export function confirm (title, text, type) {
+export function confirm (title, text, type,cancelText,confirmText,showCancelButton) {
+  
   return new Promise((resolve, reject) => {
     swal({
       title:             title,
       text:              text,
       type:              type,
-      showCancelButton:  true,
-      confirmButtonText: 'OK',
+      showCancelButton:  showCancelButton || true,
+      confirmButtonText: confirmText ||'OK',
       closeOnConfirm:    true,
-      html:              true
+      html:              true,
+      cancelButtonText: cancelText || "Cancel"
     },
     (isConfirm) => {
       resolve(isConfirm);
