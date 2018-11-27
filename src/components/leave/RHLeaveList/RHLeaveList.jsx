7c30eb9b7501717618a6f7cd48bba8a0cb229e1d
rohit kumar <rkumar@netchexonline.com>
2018-11-27 18:02:30 +0530

    rh list showing is done

---
+++ src/components/leave/RHLeaveList/RHLeaveList.jsx
@@ -0,0 +1,103 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {ButtonInfo, Button} from 'components/generic/buttons';
+
+
+const RHLeavesList = ({leave}) => {
+  let s = "Pending";
+  let f_char = s.charAt(0);
+  let leaveStatusColor = '';
+//   if (s === 'Approved') {
+    leaveStatusColor = 'green-A200';
+//   } else if (s === 'Pending') {
+//     leaveStatusColor = 'blue';
+//   } else if (s === 'Rejected') {
+//     leaveStatusColor = 'red-500';
+//   } else if (s === 'Cancelled Request') {
+//     leaveStatusColor = 'red-100';
+//   }
+  return (
+    <div className={`list-item b-l b-l-2x b-${leaveStatusColor}`}>
+      <div className="list-left">
+        <span className={`w-40 avatar ${leaveStatusColor}`}>
+          {f_char}
+        </span>
+      </div>
+      <div className="list-body">
+        <div className="pull-right text-muted text-xs">
+        </div>
+        <div>
+          <span className="_500">Holiday : {leave.name}</span>
+        </div>
+        <div>
+          <span className="_500">Date : {leave.date}</span>
+        </div>
+        
+        <div className="text-ellipsis text-muted text-sm">Month : {leave.month}</div>
+        {
+          leave.leave_type !== ''
+          ? <div className="text-ellipsis text-muted text-sm">Leave Type : {leave.type_text
+          }</div>
+          : null
+        }
+        {/* {
+          leave.late_reason !== ''
+          ? <div className="text-ellipsis text-muted text-sm">Reason For Late Applying : {leave.late_reason}</div>
+          : null
+        }
+        {
+          leave.comment !== ''
+          ? <div className="text-ellipsis text-muted text-sm">comment : {leave.comment}</div>
+          : null
+        } */}
+      </div>
+
+      {/* <div className="row">
+        {
+          leave.doc_require !== '0'
+          ? <div className="col-xs-12 row">
+            <button
+              onClick={() => { handleOpen(leave.id); }}
+              className="btn btn-info btn-responsive btn-res col-xs-2">{'Upload Leave Document'}
+            </button>
+          {
+            leave.doc_link === ''
+            ? null
+            : <div>
+              <form method="get" target="_blank" action={leave.doc_link}>
+                <button
+                  className="btn btn-primary btn-responsive btn-res col-xs-2">
+                  {'View Document'}
+                </button>
+              </form>
+            </div>
+        }
+        <div className="col-xs-12 row">
+          <small className="uploadnotic" >* Upload file size should be less than 1 MB</small>
+        </div>
+          </div>
+          : null
+        }
+      </div> */}
+    </div>
+  );
+};
+
+// MyLeavesList.PropTypes = {
+//   leave: PropTypes.shape({
+//     status:      PropTypes.string.isRequired,
+//     applied_on:  PropTypes.string.isRequired,
+//     from_date:   PropTypes.string.isRequired,
+//     to_date:     PropTypes.string.isRequired,
+//     no_of_days:  PropTypes.number.isRequired,
+//     user_Id:     PropTypes.number.isRequired,
+//     reason:      PropTypes.string.isRequired,
+//     leave_type:  PropTypes.string.isRequired,
+//     late_reason: PropTypes.string.isRequired,
+//     comment:     PropTypes.string.isRequired
+//   }).isRequired,
+//   handleOpen:  PropTypes.func.isRequired,
+//   cancelLeave: PropTypes.func.isRequired
+// };
+
+export default RHLeavesList;
--- src/modules/leave/containers/myLeaves.jsx
+++ src/modules/leave/containers/myLeaves.jsx
@@ -5,16 +5,26 @@ import Menu from 'components/generic/Menu';
 import {isNotUserValid} from 'src/services/generic';
 import Header from 'components/generic/Header';
 import UserLeavesList from 'modules/leave/components/myLeaves/UserLeavesList';
+import RHLeaves from "../components/RHLeaves/RHLeaves"
+import {  getYearArray } from 'src/services/generic';
+
 import * as actions from 'appRedux/actions';
 import * as actions_myLeaves from 'appRedux/leave/actions/myLeaves';
 
 class MyLeaves extends React.Component {
   constructor (props) {
     super(props);
+    this.state={
+      year:""
+    }
     this.props.onIsAlreadyLogin();
+
   }
   componentWillMount () {
     this.props.onMyLeavesList();
+    this.year = getYearArray();    
+    this.props.getRHList(this.year[3])
+    this.setState({year:`${this.year[3]}`});
   }
   componentWillReceiveProps (props) {
     window.scrollTo(0, 0);
@@ -22,6 +32,10 @@ class MyLeaves extends React.Component {
     if (isNotValid.status) {
       this.props.router.push(isNotValid.redirectTo);
     }
+  }
+  handleYearChange=(e)=>{
+    this.setState({ year: e.target.value });
+    this.props.getRHList(e.target.value);
   }
   render () {
     return (
@@ -32,8 +46,17 @@ class MyLeaves extends React.Component {
           <div className="app-body" id="view">
             <div className="padding">
               <div className="row">
-                <div className="col-md-12">
+                <div className="col-md-6">
                   <UserLeavesList {...this.props} />
+                </div>
+                <div className="col-md-6 rh-leave-container">
+                  <RHLeaves 
+                  stateData={this.state}
+                  yearArray={this.year}
+                  handleYearChange={this.handleYearChange}
+                  RHLeaveList={this.props.RHLeaveList}
+
+                   />
                 </div>
               </div>
             </div>
@@ -44,12 +67,13 @@ class MyLeaves extends React.Component {
   }
 }
 
-function mapStateToProps (state) {
+function mapStateToProps (state) {  
   return {
     frontend:   state.frontend.toJS(),
     loggedUser: state.logged_user.userLogin,
     userLeaves: state.userLeaves.toJS(),
-    applyLeave: state.applyLeave.toJS()
+    applyLeave: state.applyLeave.toJS(),
+    RHLeaveList:state.userLeaves.toJS().RHLeaves.rh_list
   };
 }
 const mapDispatchToProps = (dispatch) => {
@@ -62,8 +86,9 @@ const mapDispatchToProps = (dispatch) => {
     },
     onCancelLeave: (userId, from_date) => {
       return dispatch(actions_myLeaves.cancelLeave(userId, from_date));
-    }
-  };
+    },
+    getRHList:(year)=>  dispatch(actions_myLeaves.getRHList(year))
+  }
 };
 
 const VisibleMyLeaves = connect(
