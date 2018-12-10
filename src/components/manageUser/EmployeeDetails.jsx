48b1fa6a59819a8ffae9a7f076c1cacd5835c75a
reactexcel <arun.etech2011@gmail.com>
2018-12-10 13:47:17 +0530

    redesign all emp table

--- src/components/manageUser/EmployeeDetails.jsx
+++ src/components/manageUser/EmployeeDetails.jsx
@@ -10,44 +10,38 @@ const EmployeeDetails = ({ allEmpolyesDetails }) => {
 
     if (allEmpolyesDetails.length) {
         data = allEmpolyesDetails.map((employee, index) => {
-        let profileImg = isEmpty(employee.image) ? profilePlaceholder : employee.image;
+        let profileImg = isEmpty(employee.slack_profile.image_72) ? profilePlaceholder : employee.slack_profile.image_72;
         return (
             <div key={index} className="row salary-blocks-margin salary-row-bg" onClick={(e) => {}}>
-                <div className="col-md-12 salary-col-padding  employee-details">
-                    <div className="col-md-2 col-sm-2 col-xs-12 employee-profile">
-                        <div className="col-sm-12 salary-total-title">{employee.name}</div>
-                        {/* <div className="col-sm-12 salary-total-title">({employee.role_name})</div> */}
-                        <div className="col-sm-12 salary-total-title profile-image">
+                <div className="col-md-12 employee-details">
+                    <div className="col-sm-1 col-xs-12 cell profile-image">
+                        <div className="cell-item">
                             <span className="img-wrapper"><img src={profileImg} /></span>
                         </div>
-                        <div className="col-sm-12 salary-total-title">E-id : {employee.id}</div>
-                        <div className="col-sm-12 salary-total-title">{employee.jobtitle}</div>
                     </div>
-                    <div className="col-sm-1 col-xs-12 cell">
+                    <div className="col-sm-2 col-xs-12 cell">
                         {/* <div className="col-sm-12 salary-title">DOJ</div> */}
-                        <div className="col-sm-12">{employee.dateofjoining}</div>
+                        <div className="cell-item">{employee.name}</div>
+                    </div>
+                    <div className="col-sm-2 col-xs-12 cell">
+                        {/* <div className="col-sm-12 salary-title">DOJ</div> */}
+                        <div className="cell-item">{employee.jobtitle}</div>
+                    </div>
+                    <div className="col-sm-2 col-xs-12 cell">
+                        {/* <div className="col-sm-12 salary-title">DOJ</div> */}
+                        <div className="cell-item">{employee.id} - {employee.role_name}</div>
+                    </div>
+                    <div className="col-sm-2 col-xs-12 cell">
+                        {/* <div className="col-sm-12 salary-title">DOJ</div> */}
+                        <div className="cell-item">{employee.dateofjoining}</div>
                     </div>
                     <div className="col-sm-2 col-xs-12 cell">
                         {/* <div className="col-sm-12 salary-title">current_salary</div> */}
-                        <div className="col-sm-12">{employee.current_salary && `Rs.${employee.current_salary}`}</div>
-                    </div>
-                    <div className="col-sm-2 col-xs-12 cell">
-                        {/* <div className="col-sm-12 salary-title">PAN No.</div> */}
-                        <div className="col-sm-12">{employee.team}</div>
+                        <div className="cell-item">{employee.current_salary && `Rs.${employee.current_salary}`}</div>
                     </div>
                     <div className="col-sm-1 col-xs-12 cell">
-                        {/* <div className="col-sm-12 salary-title">Contact-1</div> */}
-                        <div className="col-sm-12">{employee.mobile_ph}</div>
-                    </div>
-                    <div className="col-sm-2 col-xs-12 cell">
-                        {/* <div className="col-sm-12 salary-title">Contact-1</div> */}
-                        <div className="col-sm-12">{employee.emergency_ph1}</div>
-                        <div className="col-sm-12">{employee.emergency_ph2}</div>
-                    </div>
-                    <div className="col-sm-2 col-xs-12 cell">
-                        {/* <div className="col-sm-12 salary-title">Conveyance</div> */}
-                        <div className="col-sm-12">{employee.work_email}</div>
-                        <div className="col-sm-12">{employee.work_email}</div>
+                        {/* <div className="col-sm-12 salary-title">PAN No.</div> */}
+                        <div className="cell-item">{employee.team}</div>
                     </div>
                 </div>
             </div>
@@ -57,27 +51,27 @@ const EmployeeDetails = ({ allEmpolyesDetails }) => {
     return(
         <div className="content-salary">
             <div className="row salary-blocks-margin salary-row-bg" onClick={(e) => {}}>
-                <div className="col-md-12 salary-col-padding" >
-                    <div className="col-md-2 col-sm-2 col-xs-12 employee-profile">
-                        <div className="col-sm-12 salary-total-title">Profile</div>
+                <div className="col-md-12 employee-details" >
+                    <div className="col-sm-1 col-xs-12 cell profile-image">
+                        <div className="salary-total-title">Image</div>
+                    </div>
+                    <div className="col-sm-2 col-xs-12 cell">
+                        <div className="salary-title">Name</div>
+                    </div>
+                    <div className="col-sm-2 col-xs-12 cell">
+                        <div className="salary-title">Job Title</div>
+                    </div>
+                    <div className="col-sm-2 col-xs-12 cell">
+                        <div className="salary-title">Employee Id - Role</div>
+                    </div>
+                    <div className="col-sm-2 col-xs-12 cell">
+                        <div className="salary-title">DOJ</div>
+                    </div>
+                    <div className="col-sm-2 col-xs-12 cell">
+                        <div className="salary-title">Salary</div>
                     </div>
                     <div className="col-sm-1 col-xs-12 cell">
-                        <div className="col-sm-12 salary-title">DOJ</div>
-                    </div>
-                    <div className="col-sm-2 col-xs-12 cell">
-                        <div className="col-sm-12 salary-title">Salary</div>
-                    </div>
-                    <div className="col-sm-2 col-xs-12 cell">
-                        <div className="col-sm-12 salary-title">Team</div>
-                    </div>
-                    <div className="col-sm-1 col-xs-12 cell">
-                        <div className="col-sm-12 salary-title">Mobile</div>
-                    </div>
-                    <div className="col-sm-2 col-xs-12 cell">
-                        <div className="col-sm-12 salary-title">Emergency Contact No.</div>
-                    </div>
-                    <div className="col-sm-2 col-xs-12 cell">
-                        <div className="col-sm-12 salary-title">Other details</div>
+                        <div className="salary-title">Team</div>
                     </div>
                 </div>
             </div>
