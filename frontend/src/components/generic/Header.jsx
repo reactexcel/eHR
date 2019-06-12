fb2ae44791a01f53192229e1b9374b073524c0fe
Arun <arun.etech2011@gmail.com>
2019-06-12 12:05:10 +0530

    fix to test on server

--- frontend/src/components/generic/Header.jsx
+++ frontend/src/components/generic/Header.jsx
@@ -14,15 +14,19 @@ const Header = ({pageTitle, status, showLoading, userListHeader}) => {
   }
   return (
     <div className="app-header white box-shadow m-b">
-      <div className="navbar" onClick={()=>{console.log("navvv")}}>
+      <div className="navbar">
         <a /* data-toggle="modal" data-target="#aside" */ className="navbar-item pull-left hidden-md hidden-lg" 
-          onClick={()=>{
+        //   onClick={()=>{
+        //   if($('#aside')[0].style.display === "" || 'none'){
+        //     $('#aside').addClass(' in show')
+        //   }
+        // }}
+          >
+          <i className="material-icons" onClick={()=>{
           if($('#aside')[0].style.display === "" || 'none'){
             $('#aside').addClass(' in show')
           }
-        }}
-          >
-          <i className="material-icons">&#xe5d2;</i>
+        }}>&#xe5d2;</i>
         </a>
         <div className="navbar-item pull-left h5" style={{marginTop: '-4px'}} id="pageTitle">{pageTitle}{status || null}</div>
         {userListHeaderIcon}
